2fa5d43500add4d89af5e85cd9328781632b45a7
Shigma <1700011071@pku.edu.cn>
2019-12-13 02:52:30 +0800

    Update publish.ts

--- build/publish.ts
+++ build/publish.ts
@@ -5,11 +5,10 @@ import globby from 'globby'
 
 const { CI, GITHUB_EVENT_NAME, GITHUB_REF } = process.env
 
-// NOTE: Only For Test
-// if (CI && (GITHUB_REF !== 'refs/heads/master' || GITHUB_EVENT_NAME !== 'push')) {
-//   console.log('publish skipped.')
-//   process.exit(0)
-// }
+if (CI && (GITHUB_REF !== 'refs/heads/master' || GITHUB_EVENT_NAME !== 'push')) {
+  console.log('publish skipped.')
+  process.exit(0)
+}
 
 console.log(Object.keys(process.env).join('\n'))
 
@@ -25,11 +24,11 @@ const cwd = resolve(__dirname, '..')
   for (const name of folders) {
     const meta: PackageJSON = require(`../${name}/package`)
     if (meta.private) continue
-    // try {
-    //   const version = await exec(`npm show ${meta.name} version`)
-    //   if (lte(meta.version, version)) continue
-    // } catch {}
+    try {
+      const version = await exec(`npm show ${meta.name} version`)
+      if (lte(meta.version, version)) continue
+    } catch {}
     console.log(`publishing ${name}@${meta.version} ...`)
-    await exec(`yarn publish ${name}`, { cwd })
+    await exec(`yarn publish ${name}`, { cwd }).catch(() => {})
   }
 })()
