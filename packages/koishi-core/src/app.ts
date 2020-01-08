5a31c6e20867d738bef3005fc38c4f5bb3b68b63
Shigma <1700011071@pku.edu.cn>
2020-01-08 23:52:08 +0800

    chore: revert after-middlewares hook

--- packages/koishi-core/src/app.ts
+++ packages/koishi-core/src/app.ts
@@ -400,7 +400,6 @@ export class App extends Context {
 
     // update middleware set
     this._middlewareSet.delete(counter)
-    this.emitEvent(meta, 'after-middlewares', meta)
 
     // flush user & group data
     await meta.$user?._update()
