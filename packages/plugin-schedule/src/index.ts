273d003b178cb684e61eeadb8d340509525d5ee6
Shigma <1700011071@pku.edu.cn>
2020-01-11 22:35:39 +0800

    plugin-schedule: optimize

--- packages/plugin-schedule/src/index.ts
+++ packages/plugin-schedule/src/index.ts
@@ -1,8 +1,10 @@
-import { Context, onStart, appMap } from 'koishi-core'
+import { Context, appMap, Database, CommandConfig } from 'koishi-core'
 import { Schedule } from './database'
+import './database'
 
-function inspectSchedule ({ id, assignee, meta, interval, command, time }: Schedule, now = Date.now()) {
+function inspectSchedule ({ id, assignee, meta, interval, command, time }: Schedule) {
   if (!appMap[assignee]) return
+  const now = Date.now()
   const app = appMap[assignee]
 
   if (!interval) {
@@ -30,27 +32,33 @@ function inspectSchedule ({ id, assignee, meta, interval, command, time }: Sched
   }
 }
 
-export function apply (ctx: Context) {
-  onStart(async ({ database }) => {
-    const now = Date.now()
-    const schedules = await database.getAllSchedules()
-    schedules.forEach(schedule => inspectSchedule(schedule, now))
+const databases = new Set<Database>()
+
+export const name = 'schedule'
+
+export function apply (ctx: Context, config: CommandConfig = {}) {
+  const { database } = ctx.app
+
+  ctx.app.receiver.on('connect', async () => {
+    if (!database || databases.has(database)) return
+    databases.add(database)
+    const schedules = await database?.getAllSchedules()
+    schedules?.forEach(schedule => inspectSchedule(schedule))
   })
 
-  ctx.command('advanced')
-    .subcommand('schedule <time> -- <command>', '设置定时命令', { authority: 3, maxUsage: 5 })
+  ctx.command('schedule <time> -- <command>', '设置定时命令', { authority: 3, maxUsage: 5, ...config })
     .option('-i, --interval <interval>', '设置触发的间隔秒数', { default: 0, authority: 4 })
     // .option('-l, --list', '查看已经设置的日程')
     .option('-d, --delete <id>', '删除已经设置的日程', { notUsage: true })
     .action(async ({ meta, options, rest }, date) => {
       if (options.delete) {
-        await ctx.app.database.removeSchedule(options.delete)
+        await database.removeSchedule(options.delete)
         return meta.$send('日程已删除。')
       }
 
       const time = date ? new Date(date).valueOf() : Date.now()
-      const schedule = await ctx.app.database.createSchedule(time, ctx.app.options.selfId, options.interval * 1000, rest, meta)
-      inspectSchedule(schedule)
-      return meta.$send(`日程已创建，编号为 ${schedule.id}。`)
+      const schedule = await database.createSchedule(time, ctx.app.selfId, options.interval * 1000, rest, meta)
+      await meta.$send(`日程已创建，编号为 ${schedule.id}。`)
+      return inspectSchedule(schedule)
     })
 }
