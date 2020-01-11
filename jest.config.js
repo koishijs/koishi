2487a757bb5566894b642ed04a0aec923c7c3284
Shigma <1700011071@pku.edu.cn>
2020-01-11 18:25:22 +0800

    chore: adjust dependencies

--- jest.config.js
+++ jest.config.js
@@ -10,7 +10,7 @@ module.exports = {
   testEnvironment: 'node',
   moduleFileExtensions: ['ts', 'js', 'json'],
   moduleNameMapper: {
-    "koishi-(test-utils|database-[\\w-]+)": "<rootDir>/packages/$1/src",
+    "koishi-(test-utils|(database|plugin)-[\\w-]+)": "<rootDir>/packages/$1/src",
     "koishi-[\\w-]+": "<rootDir>/packages/$0/src",
   },
   coverageReporters: ['text', 'lcov'],
