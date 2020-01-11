73e3d0edf08a76f5094c0bfde0e0221bcab3fed8
Shigma <1700011071@pku.edu.cn>
2020-01-12 02:19:37 +0800

    build: attach rest argv

--- build/start.ts
+++ build/start.ts
@@ -4,7 +4,7 @@ import spawn from 'cross-spawn'
 const name = process.argv[2]
 if (!name) process.exit(0)
 
-spawn('npx', ['koishi', 'run'], {
+spawn('npx', ['koishi', 'run', ...process.argv.slice(3)], {
   cwd: resolve(__dirname, '../bots', name),
   stdio: 'inherit',
 })
