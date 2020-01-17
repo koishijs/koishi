19b540e5c49cc8b4ef3170551900a9b6c884f800
Shigma <1700011071@pku.edu.cn>
2020-01-17 18:05:04 +0800

    optimize nlp plugin

--- packages/plugin-nlp/src/index.ts
+++ packages/plugin-nlp/src/index.ts
@@ -1,16 +1,11 @@
 import { Command, Meta, Context, ParsedLine, ParsedCommandLine } from 'koishi-core'
 import { tag } from 'nodejieba'
+import { resolve } from 'path'
 
 declare module 'koishi-core/dist/command' {
   interface Command {
     intend (keyword: string, callback: IntenderCallback): void
     intend (keywords: string[], callback: IntenderCallback): void
-  }
-}
-
-declare module 'koishi-core/dist/parser' {
-  interface ParsedLine {
-    confidence?: number
   }
 }
 
@@ -20,20 +15,31 @@ declare module 'koishi-core/dist/meta' {
   }
 }
 
-type IntenderCallback = (meta: Meta, keyword: string) => ParsedLine
+export interface Intension extends Partial<ParsedLine> {
+  confidence?: number
+}
 
+export type IntenderCallback = (meta: Meta, keyword: string) => Intension
+
+// TODO: pending nodejieba typings
 interface TagResult {
   word: string
   tag: string
 }
 
-interface Intender {
+export interface Intender {
   command: Command
   keywords: string[]
   callback: IntenderCallback
 }
 
-interface NlpConfig {
+export interface NlpConfig {
+  // TODO: pending nodejieba typings
+  dict?: string
+  hmmDict?: string
+  userDict?: string
+  idfDict?: string
+  stopWordDict?: string
   threshold?: number
 }
 
@@ -46,22 +52,44 @@ Command.prototype.intend = function (this: Command, arg0: string | string[], cal
 
 export const name = 'nlp'
 
+const cwd = process.cwd()
+
+function resolvePathConfig (value?: string) {
+  if (value) return resolve(cwd, value)
+}
+
 export function apply (ctx: Context, options: NlpConfig = {}) {
   options = { threshold: 0.5, ...options }
+  options.dict = resolvePathConfig(options.dict)
+  options.hmmDict = resolvePathConfig(options.hmmDict)
+  options.idfDict = resolvePathConfig(options.idfDict)
+  options.userDict = resolvePathConfig(options.userDict)
+  options.stopWordDict = resolvePathConfig(options.stopWordDict)
 
   ctx.middleware((meta, next) => {
-    let confidence = options.threshold, bestFit: ParsedCommandLine
+    let max = options.threshold
+    let bestFit: ParsedCommandLine
+
     for (const { keywords, callback, command } of intenders) {
+      // find matched keyword
       const keyword = keywords.find(k => meta.message.includes(k))
-      if (keyword) {
-        if (!meta.$tags) meta.$tags = tag(meta.message)
-        const argv = callback(meta, keyword)
-        if (argv.confidence > confidence) {
-          confidence = argv.confidence
-          bestFit = { meta, command, ...argv }
-        }
+      if (!keyword) continue
+
+      // attach word tags
+      if (!meta.$tags) meta.$tags = tag(meta.message)
+
+      // generate intension
+      const intension = callback(meta, keyword)
+      if (!intension) return
+
+      // find most credible intension
+      const confidence = intension.confidence ?? 1
+      if (confidence > max) {
+        max = confidence
+        bestFit = { meta, command, ...intension }
       }
     }
+
     if (bestFit) {
       return bestFit.command.execute(bestFit, next)
     } else {
