2eac63ba65374824815596ad9d2f46b48d0b05fc
Shigma <1700011071@pku.edu.cn>
2020-01-19 01:22:31 +0800

    test(plugin-common): exit command

--- packages/plugin-common/src/exit.ts
+++ packages/plugin-common/src/exit.ts
@@ -1,13 +1,10 @@
 import { Context } from 'koishi-core'
 
-const CODE_STOP = 0
-const CODE_RESTART = 1
-
 export default function apply (ctx: Context) {
   ctx.command('exit', '停止机器人运行', { authority: 4 })
-    .option('-c, --code [code]', '设置 exit code', { default: 0 })
-    .shortcut('关机', { prefix: true, options: { code: CODE_STOP } })
-    .shortcut('重启', { prefix: true, options: { code: CODE_RESTART } })
+    .option('-c, --code [code]', '设置 exit code')
+    .shortcut('关机', { prefix: true, options: { code: 0 } })
+    .shortcut('重启', { prefix: true, options: { code: -1 } })
     .action(({ options }) => {
       process.exit(+options.code)
     })
