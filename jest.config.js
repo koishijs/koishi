ba28276c89f0ab0806aed934381c1f2590f197a1
Shigma <1700011071@pku.edu.cn>
2020-01-23 16:35:12 +0800

    chore: update module name mapper

--- jest.config.js
+++ jest.config.js
@@ -12,6 +12,7 @@ module.exports = {
   moduleNameMapper: {
     "koishi-(test-utils|(database|plugin)-[\\w-]+)(/dist)?": "<rootDir>/packages/$1/src",
     "koishi-[\\w-]+": "<rootDir>/packages/$0/src",
+    "shiki-universe": "<rootDir>/bots/shiki/lib/shiki-universe/src",
   },
   coverageReporters: ['text', 'lcov'],
   coveragePathIgnorePatterns: [
