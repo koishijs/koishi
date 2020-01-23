533f4defc2ddcfb3064ca9a5b4af88eb42a1ac7c
Shigma <1700011071@pku.edu.cn>
2020-01-23 16:33:48 +0800

    feat(test-utils): experimental random mocks api

--- packages/test-utils/src/index.ts
+++ packages/test-utils/src/index.ts
@@ -1,5 +1,15 @@
+import * as utils from './random'
+
+export { utils }
+
 export * from './app'
 export * from './database'
 export * from './mocks'
 export * from './utils'
 export * from './server'
+
+jest.mock('koishi-utils', () => {
+  const utils1 = jest.requireActual('koishi-utils')
+  const utils2 = jest.requireActual('./random')
+  return { ...utils1, ...utils2 }
+})
