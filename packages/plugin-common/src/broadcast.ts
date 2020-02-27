baf7f1f662e6d3684e8c38bd30dc652a0baf0600
Shigma <1700011071@pku.edu.cn>
2020-02-27 17:43:17 +0800

    feat(plugin-common): support broadcast -o

--- packages/plugin-common/src/broadcast.ts
+++ packages/plugin-common/src/broadcast.ts
@@ -9,11 +9,27 @@ const defaultOptions: BroadcastOptions = {
   broadcastInterval: 1000,
 }
 
-export default function apply (ctx: Context, options: BroadcastOptions = {}) {
-  options = { ...defaultOptions, ...options }
+export default function apply (ctx: Context, config: BroadcastOptions = {}) {
+  config = { ...defaultOptions, ...config }
+
+  async function broadcast (selfId: string | number, groups: number[], message: string) {
+    const { sender } = appMap[selfId]
+    for (let index = 0; index < groups.length; index++) {
+      if (index) await sleep(config.broadcastInterval)
+      sender.sendGroupMsgAsync(groups[index], message)
+    }
+  }
 
   ctx.command('broadcast <message...>', '全服广播', { authority: 4 })
-    .action(async (_, message: string) => {
+    .option('-o, --only', '仅向当前 Bot 负责的群进行广播')
+    .action(async ({ options, meta }, message) => {
+      if (!message) return meta.$send('请输入要发送的文本。')
+
+      if (options.only) {
+        const groups = await ctx.database.getAllGroups(['id'], [ctx.app.selfId])
+        return broadcast(ctx.app.selfId, groups.map(g => g.id), message)
+      }
+
       const groups = await ctx.database.getAllGroups(['id', 'assignee'])
       const assignMap: Record<number, number[]> = {}
       for (const { id, assignee } of groups) {
@@ -23,13 +39,6 @@ export default function apply (ctx: Context, options: BroadcastOptions = {}) {
           assignMap[assignee].push(id)
         }
       }
-      Object.keys(assignMap).forEach(async (id: any) => {
-        const groups = assignMap[id]
-        const { sender } = appMap[id]
-        for (let index = 0; index < groups.length; index++) {
-          if (index) await sleep(options.broadcastInterval)
-          sender.sendGroupMsgAsync(groups[index], message)
-        }
-      })
+      return Promise.all(Object.keys(assignMap).map(id => broadcast(id, assignMap[id], message)))
     })
 }
