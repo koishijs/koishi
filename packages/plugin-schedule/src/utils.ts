c9942b6a641511aeef7bbc6bd9aa799237e0805e
Shigma <1700011071@pku.edu.cn>
2020-03-15 02:19:39 +0800

    support time unit

---
+++ packages/plugin-schedule/src/utils.ts
@@ -0,0 +1,45 @@
+
+
+const second = 1000
+const minute = second * 60
+const hour = minute * 60
+const day = hour * 24
+const week = day * 7
+
+const timeRegExp = /^(\d+(?:\.\d+)?w(?:eek(?:s)?)?)?(\d+(?:\.\d+)?d(?:ay(?:s)?)?)?(\d+(?:\.\d+)?h(?:our(?:s)?)?)?(\d+(?:\.\d+)?m(?:in(?:ute)?(?:s)?)?)?(\d+(?:\.\d+)?s(?:ec(?:ond)?(?:s)?)?)?$/
+
+export function parseTime (source: string) {
+  const capture = timeRegExp.exec(source)
+  if (!capture) return 0
+  return (parseFloat(capture[1]) * week || 0)
+    + (parseFloat(capture[2]) * day || 0)
+    + (parseFloat(capture[3]) * hour || 0)
+    + (parseFloat(capture[4]) * minute || 0)
+    + (parseFloat(capture[5]) * second || 0)
+}
+
+export function formatTime (ms: number) {
+  let result: string
+  if (ms >= day - hour / 2) {
+    ms += hour / 2
+    result = Math.floor(ms / day) + ' 天'
+    if (ms % day > hour) {
+      result += ` ${Math.floor(ms % day / hour)} 小时`
+    }
+  } else if (ms >= hour - minute / 2) {
+    ms += minute / 2
+    result = Math.floor(ms / hour) + ' 小时'
+    if (ms % hour > minute) {
+      result += ` ${Math.floor(ms % hour / minute)} 分钟`
+    }
+  } else if (ms >= minute - second / 2) {
+    ms += second / 2
+    result = Math.floor(ms / minute) + ' 分钟'
+    if (ms % minute > second) {
+      result += ` ${Math.floor(ms % minute / second)} 秒`
+    }
+  } else {
+    result = Math.round(ms / second) + ' 秒'
+  }
+  return result
+}
