c04efc7c907d466d8c3e2c8d053d99a641427eee
Shigma <1700011071@pku.edu.cn>
2020-03-28 03:07:18 +0800

    setTimezoneOffset

--- packages/koishi-utils/src/date.ts
+++ packages/koishi-utils/src/date.ts
@@ -1,9 +1,17 @@
-export function getDateNumber (date: number | Date = new Date()) {
-  if (typeof date === 'number') date = new Date(date)
-  return Math.floor(Date.UTC(date.getFullYear(), date.getMonth(), date.getDate()) / 86400000)
+let timezoneOffset = new Date().getTimezoneOffset()
+
+export function setTimezoneOffset (offset: number) {
+  timezoneOffset = offset
 }
 
-export function fromDateNumber (value: number) {
+export function getDateNumber (date: number | Date = new Date(), offset?: number) {
+  if (typeof date === 'number') date = new Date(date)
+  if (offset === undefined) offset = timezoneOffset
+  return Math.floor((date.valueOf() / 60000 - offset) / 1440)
+}
+
+export function fromDateNumber (value: number, offset?: number) {
   const date = new Date(value * 86400000)
-  return new Date(date.getFullYear(), date.getMonth(), date.getDate())
+  if (offset === undefined) offset = timezoneOffset
+  return new Date(+date + offset * 60000)
 }
