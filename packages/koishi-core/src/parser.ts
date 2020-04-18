10698d26f23a5dc148d973f981fbfc16f343a39b
Shigma <1700011071@pku.edu.cn>
2020-04-18 18:41:22 +0800

    fix(core): fix non-negated options

--- packages/koishi-core/src/parser.ts
+++ packages/koishi-core/src/parser.ts
@@ -193,7 +193,7 @@ export function parseLine (source: string, argsDef: CommandArgument[], optsDef: 
     for (; i < arg.length; ++i) {
       if (arg.charCodeAt(i) !== 45) break
     }
-    if (arg.slice(i, i + 3) === 'no-') {
+    if (arg.slice(i, i + 3) === 'no-' && !optsDef[arg.slice(i + 3)]) {
       name = arg.slice(i + 3)
       handleOption(name, true, false)
       continue
