26d0236f229708be60cc52bc0dcf9950444c4039
Shigma <1700011071@pku.edu.cn>
2020-04-18 21:28:56 +0800

    bump version

--- packages/koishi-core/src/command.ts
+++ packages/koishi-core/src/command.ts
@@ -417,7 +417,7 @@ export class Command {
         for (; i < arg.length; ++i) {
           if (arg.charCodeAt(i) !== 45) break
         }
-        if (arg.slice(i, i + 3) === 'no-' && !this._optionMap[arg.slice(i + 3)]) {
+        if (arg.slice(i, i + 3) === 'no-' && !this._optionMap[arg.slice(i)]) {
           name = arg.slice(i + 3)
           handleOption(name, true, false)
           continue
