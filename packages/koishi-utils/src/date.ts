5fd44f89d2e197f03fcd23523cf8003fbea9dee6
Shigma <1700011071@pku.edu.cn>
2020-07-19 01:50:46 +0800

    chore: bump versions

--- packages/koishi-utils/src/date.ts
+++ packages/koishi-utils/src/date.ts
@@ -8,22 +8,24 @@ export function getTimezoneOffset () {
   return timezoneOffset
 }
 
-const second = 1000
-const minute = second * 60
-const hour = minute * 60
-const day = hour * 24
-const week = day * 7
+export namespace Time {
+  export const second = 1000
+  export const minute = second * 60
+  export const hour = minute * 60
+  export const day = hour * 24
+  export const week = day * 7
+}
 
 export function getDateNumber (date: number | Date = new Date(), offset?: number) {
   if (typeof date === 'number') date = new Date(date)
   if (offset === undefined) offset = timezoneOffset
-  return Math.floor((date.valueOf() / minute - offset) / 1440)
+  return Math.floor((date.valueOf() / Time.minute - offset) / 1440)
 }
 
 export function fromDateNumber (value: number, offset?: number) {
-  const date = new Date(value * day)
+  const date = new Date(value * Time.day)
   if (offset === undefined) offset = timezoneOffset
-  return new Date(+date + offset * minute)
+  return new Date(+date + offset * Time.minute)
 }
 
 const timeRegExp = /^(\d+(?:\.\d+)?w(?:eek(?:s)?)?)?(\d+(?:\.\d+)?d(?:ay(?:s)?)?)?(\d+(?:\.\d+)?h(?:our(?:s)?)?)?(\d+(?:\.\d+)?m(?:in(?:ute)?(?:s)?)?)?(\d+(?:\.\d+)?s(?:ec(?:ond)?(?:s)?)?)?$/
@@ -31,11 +33,11 @@ const timeRegExp = /^(\d+(?:\.\d+)?w(?:eek(?:s)?)?)?(\d+(?:\.\d+)?d(?:ay(?:s)?)?
 export function parseTime (source: string) {
   const capture = timeRegExp.exec(source)
   if (!capture) return 0
-  return (parseFloat(capture[1]) * week || 0)
-    + (parseFloat(capture[2]) * day || 0)
-    + (parseFloat(capture[3]) * hour || 0)
-    + (parseFloat(capture[4]) * minute || 0)
-    + (parseFloat(capture[5]) * second || 0)
+  return (parseFloat(capture[1]) * Time.week || 0)
+    + (parseFloat(capture[2]) * Time.day || 0)
+    + (parseFloat(capture[3]) * Time.hour || 0)
+    + (parseFloat(capture[4]) * Time.minute || 0)
+    + (parseFloat(capture[5]) * Time.second || 0)
 }
 
 export function parseDate (date: string) {
@@ -52,26 +54,26 @@ export function parseDate (date: string) {
 
 export function formatTime (ms: number) {
   let result: string
-  if (ms >= day - hour / 2) {
-    ms += hour / 2
-    result = Math.floor(ms / day) + ' 天'
-    if (ms % day > hour) {
-      result += ` ${Math.floor(ms % day / hour)} 小时`
+  if (ms >= Time.day - Time.hour / 2) {
+    ms += Time.hour / 2
+    result = Math.floor(ms / Time.day) + ' 天'
+    if (ms % Time.day > Time.hour) {
+      result += ` ${Math.floor(ms % Time.day / Time.hour)} 小时`
     }
-  } else if (ms >= hour - minute / 2) {
-    ms += minute / 2
-    result = Math.floor(ms / hour) + ' 小时'
-    if (ms % hour > minute) {
-      result += ` ${Math.floor(ms % hour / minute)} 分钟`
+  } else if (ms >= Time.hour - Time.minute / 2) {
+    ms += Time.minute / 2
+    result = Math.floor(ms / Time.hour) + ' 小时'
+    if (ms % Time.hour > Time.minute) {
+      result += ` ${Math.floor(ms % Time.hour / Time.minute)} 分钟`
     }
-  } else if (ms >= minute - second / 2) {
-    ms += second / 2
-    result = Math.floor(ms / minute) + ' 分钟'
-    if (ms % minute > second) {
-      result += ` ${Math.floor(ms % minute / second)} 秒`
+  } else if (ms >= Time.minute - Time.second / 2) {
+    ms += Time.second / 2
+    result = Math.floor(ms / Time.minute) + ' 分钟'
+    if (ms % Time.minute > Time.second) {
+      result += ` ${Math.floor(ms % Time.minute / Time.second)} 秒`
     }
   } else {
-    result = Math.round(ms / second) + ' 秒'
+    result = Math.round(ms / Time.second) + ' 秒'
   }
   return result
 }
@@ -81,9 +83,9 @@ const dayMap = ['日', '一', '二', '三', '四', '五', '六']
 export function formatTimeInterval (time: Date, interval: number) {
   if (!interval) {
     return time.toLocaleString()
-  } else if (interval === day) {
+  } else if (interval === Time.day) {
     return `每天 ${time.toLocaleTimeString()}`
-  } else if (interval === week) {
+  } else if (interval === Time.week) {
     return `每周${dayMap[time.getDay()]} ${time.toLocaleTimeString()}`
   } else {
     return `${time.toLocaleString()} 起每隔 ${formatTime(interval)}`
