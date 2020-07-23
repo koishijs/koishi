33e19fb764eeeb28810d3e35a2b24219f0ae0f1a
Shigma <1700011071@pku.edu.cn>
2020-07-24 01:00:26 +0800

    status: optimize output

--- packages/plugin-status/src/index.ts
+++ packages/plugin-status/src/index.ts
@@ -1,41 +1,10 @@
 import { Context } from 'koishi-core'
-import plugin, { getStatus, StatusOptions } from './status'
+import plugin, { StatusOptions } from './status'
 
 export * from './status'
 
-const startTime = new Date().toLocaleString()
-
 export const name = 'status'
 
-const defaultConfig: StatusOptions = {
-  sort: () => 0,
-}
-
 export async function apply (ctx: Context, config: StatusOptions = {}) {
-  config = { ...defaultConfig, ...config }
-
-  ctx.app.plugin(plugin)
-
-  ctx.command('status', '查看机器人运行状态')
-    .shortcut('你的状态', { prefix: true })
-    .shortcut('你的状况', { prefix: true })
-    .shortcut('运行情况', { prefix: true })
-    .shortcut('运行状态', { prefix: true })
-    .action(async ({ meta }) => {
-      const { apps, cpu, memory } = await getStatus(config)
-
-      const output = apps.sort(config.sort).map(({ label, selfId, code, rate }) => {
-        return `${label || selfId}：${code ? '无法连接' : `工作中（${rate}/min）`}`
-      })
-
-      output.push('==========')
-
-      output.push(
-        `启动时间：${startTime}`,
-        `CPU 使用率：${(cpu.app * 100).toFixed()}% / ${(cpu.total * 100).toFixed()}%`,
-        `内存使用率：${(memory.app * 100).toFixed()}% / ${(memory.total * 100).toFixed()}%`,
-      )
-
-      return meta.$send(output.join('\n'))
-    })
+  ctx.plugin(plugin, config)
 }
--- packages/plugin-status/src/status.ts
+++ packages/plugin-status/src/status.ts
@@ -1,4 +1,4 @@
-import { appList, App, Command, getSelfIds } from 'koishi-core'
+import { appList, App, Command, Context } from 'koishi-core'
 import { cpus, totalmem, freemem } from 'os'
 import {} from 'koishi-database-mysql'
 
@@ -73,6 +73,7 @@ export interface Status {
   memory: Rate
   cpu: Rate
   timestamp: number
+  startTime: number
 }
 
 export interface AppStatus {
@@ -89,12 +90,11 @@ export function extendStatus (callback: StatusModifier) {
   statusModifiers.push(callback)
 }
 
-async function _getStatus (config: StatusOptions) {
-  const assignees = await getSelfIds()
+async function _getStatus (config: StatusOptions, extend: boolean) {
   const [[[{ 'COUNT(*)': userCount }], [{ 'COUNT(*)': groupCount }]], apps] = await Promise.all([
     appList[0].database.mysql.query<[{ 'COUNT(*)': number }][]>([
-      `SELECT COUNT(*) FROM \`user\` WHERE 'CURRENT_TIMESTAMP() - \`lastCall\` < 1000 * 3600 * 24'`,
-      `SELECT COUNT(*) FROM \`group\` WHERE \`assignee\` IN (${assignees.join(',')})`,
+      `SELECT COUNT(*) FROM \`user\` WHERE CURRENT_TIMESTAMP() - \`lastCall\` < 1000 * 3600 * 24`,
+      `SELECT COUNT(*) FROM \`group\` WHERE \`assignee\``,
     ].join(';')),
     Promise.all(appList.map<Promise<AppStatus>>(async (app) => ({
       label: app.options.label,
@@ -105,9 +105,9 @@ async function _getStatus (config: StatusOptions) {
   ])
   const memory = memoryRate()
   const cpu = { app: appRate, total: usedRate }
-  const status: Status = { apps, userCount, groupCount, memory, cpu, timestamp }
-  for (const modifier of statusModifiers) {
-    await modifier(status, config)
+  const status: Status = { apps, userCount, groupCount, memory, cpu, timestamp, startTime }
+  if (extend) {
+    await Promise.all(statusModifiers.map(modifier => modifier(status, config)))
   }
   return status
 }
@@ -115,14 +115,23 @@ async function _getStatus (config: StatusOptions) {
 let cachedStatus: Promise<Status>
 let timestamp: number
 
-export async function getStatus (config: StatusOptions): Promise<Status> {
+export async function getStatus (config: StatusOptions, extend = false): Promise<Status> {
   const now = Date.now()
   if (now - timestamp < 60000) return cachedStatus
   timestamp = now
-  return cachedStatus = _getStatus(config)
+  return cachedStatus = _getStatus(config, extend)
 }
 
-export default function apply (app: App, config: StatusOptions = {}) {
+const startTime = Date.now()
+
+const defaultConfig: StatusOptions = {
+  sort: () => 0,
+}
+
+export default function apply (ctx: Context, config: StatusOptions) {
+  const app = ctx.app
+  config = { ...defaultConfig, ...config }
+
   app.on('before-command', ({ meta }) => {
     meta.$user['lastCall'] = new Date()
   })
@@ -144,7 +153,7 @@ export default function apply (app: App, config: StatusOptions = {}) {
 
     if (!app.server.router) return
     app.server.router.get('/status', async (ctx) => {
-      const status = await getStatus(config).catch<Status>((error) => {
+      const status = await getStatus(config, true).catch<Status>((error) => {
         appList[0].logger('status').warn(error)
         return null
       })
@@ -158,4 +167,29 @@ export default function apply (app: App, config: StatusOptions = {}) {
   app.on('before-disconnect', () => {
     clearInterval(timer)
   })
+
+  ctx.command('status', '查看机器人运行状态')
+    .shortcut('你的状态', { prefix: true })
+    .shortcut('你的状况', { prefix: true })
+    .shortcut('运行情况', { prefix: true })
+    .shortcut('运行状态', { prefix: true })
+    .action(async ({ meta }) => {
+      const { apps, cpu, memory, startTime, userCount, groupCount } = await getStatus(config)
+
+      const output = apps.sort(config.sort).map(({ label, selfId, code, rate }) => {
+        return `${label || selfId}：${code ? '无法连接' : `工作中（${rate}/min）`}`
+      })
+
+      output.push('==========')
+
+      output.push(
+        `活跃用户数量：${userCount}`,
+        `活跃群数量：${groupCount}`,
+        `启动时间：${new Date(startTime).toLocaleString('zh-CN', { hour12: false })}`,
+        `CPU 使用率：${(cpu.app * 100).toFixed()}% / ${(cpu.total * 100).toFixed()}%`,
+        `内存使用率：${(memory.app * 100).toFixed()}% / ${(memory.total * 100).toFixed()}%`,
+      )
+
+      return meta.$send(output.join('\n'))
+    })
 }
