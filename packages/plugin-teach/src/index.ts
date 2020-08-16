ab4dec39794572789c3d76079baac9bcda28f2f1
Shigma <1700011071@pku.edu.cn>
2020-08-16 22:46:32 +0800

    teach: add config prefix

--- packages/plugin-teach/src/index.ts
+++ packages/plugin-teach/src/index.ts
@@ -1,6 +1,6 @@
 /* eslint-disable no-irregular-whitespace */
 
-import { Context, ExecuteArgv, Session } from 'koishi-core'
+import { Context, ExecuteArgv } from 'koishi-core'
 import { Dialogue, parseTeachArgs } from './utils'
 import internal from './internal'
 import receiver from './receiver'
@@ -16,6 +16,7 @@ import successor from './plugins/successor'
 import time from './plugins/time'
 import writer from './plugins/writer'
 import database from './database'
+import escapeStringRegexp from 'escape-string-regexp'
 
 export * from './database'
 export * from './utils'
@@ -31,7 +32,7 @@ export * from './plugins/successor'
 export * from './plugins/time'
 export * from './plugins/writer'
 
-export type Options = Dialogue.Config
+export type Config = Dialogue.Config
 
 declare module 'koishi-core/dist/context' {
   interface EventMap {
@@ -40,20 +41,21 @@ declare module 'koishi-core/dist/context' {
   }
 }
 
-const cheatSheet = ({ $user: { authority } }: Session<'authority'>) => `\
+const cheatSheet = (p: string, authority: number) => `\
 教学系统基本用法：
-　添加问答：# 问题 回答
-　搜索回答：## 问题
-　搜索问题：## ~ 回答
-　查看问答：#id
-　修改问题：#id 问题
-　修改回答：#id ~ 回答
-　删除问答：#id -r
+　添加问答：${p} 问题 回答
+　搜索回答：${p}${p} 问题
+　搜索问题：${p}${p} ~ 回答
+　查看问答：${p}id
+　修改问题：${p}id 问题
+　修改回答：${p}id ~ 回答
+　删除问答：${p}id -r
+　批量查看：${p}${p}id
 搜索选项：
 　管道语法：　　　|
 　结果页码：　　　/ page
 　禁用递归查询：　-R${authority >= 3 ? `
-　正则+合并结果：###` : ''}
+　正则+合并结果：${p}${p}${p}` : ''}
 上下文选项：
 　允许本群：　　　-e
 　禁止本群：　　　-d${authority >= 3 ? `
@@ -99,10 +101,13 @@ const cheatSheet = ({ $user: { authority } }: Session<'authority'>) => `\
 
 export const name = 'teach'
 
-//                      $1  $2
-const teachRegExp = /^#(#?)((\d+(?:\.\.\d+)?(?:,\d+(?:\.\.\d+)?)*)?|#?)(\s+|$)/
-
 export function apply(ctx: Context, config: Dialogue.Config = {}) {
+  const { prefix = '#' } = config
+  const g = '\\d+(?:\\.\\.\\d+)?'
+  const p = escapeStringRegexp(prefix)
+  const teachRegExp = new RegExp(`^${p}(${p}?)((${g}(?:,${g})*)?|${p}?)(\\s+|$)`)
+  //                                   $1     $2
+
   ctx.on('parse', (source, session, builtin) => {
     if (builtin && session.$prefix) return
     const capture = source.match(teachRegExp)
@@ -113,9 +118,9 @@ export function apply(ctx: Context, config: Dialogue.Config = {}) {
     const { options, args, rest } = command.parse(message)
     const argv: ExecuteArgv = { options, args, command, source, rest }
 
-    if (capture[1] === '#') {
+    if (capture[1] === prefix) {
       options['search'] = true
-      if (capture[2] === '#') {
+      if (capture[2] === prefix) {
         options['autoMerge'] = true
         options['regexp'] = true
       }
@@ -123,7 +128,7 @@ export function apply(ctx: Context, config: Dialogue.Config = {}) {
       options['help'] = true
     }
 
-    if (capture[2] && capture[2] !== '#') {
+    if (capture[2] && capture[2] !== prefix) {
       options['target'] = capture[2]
     }
 
@@ -133,7 +138,7 @@ export function apply(ctx: Context, config: Dialogue.Config = {}) {
 
   ctx.command('teach [question] [answer]', '添加教学对话', { authority: 2, checkUnknown: true, hideOptions: true })
     .userFields(['authority', 'id'])
-    .usage(cheatSheet)
+    .usage(({ $user }) => cheatSheet(prefix, $user.authority))
     .action(async ({ options, session, args }) => {
       const argv: Dialogue.Argv = { ctx, session, args, config, options }
       return ctx.bail('dialogue/validate', argv)
