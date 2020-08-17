829507275b5e8405d9ad1c15517a6f29207c11ab
Shigma <1700011071@pku.edu.cn>
2020-08-17 13:00:03 +0800

    use broadcast api

--- packages/plugin-rss/src/index.ts
+++ packages/plugin-rss/src/index.ts
@@ -1,4 +1,4 @@
-import { Context, Group } from 'koishi-core'
+import { Context } from 'koishi-core'
 import { Logger, Time } from 'koishi-utils'
 import RssFeedEmitter from 'rss-feed-emitter'
 
@@ -18,26 +18,35 @@ export const name = 'rss'
 
 export function apply(ctx: Context, config: Config = {}) {
   const { refresh = Time.minute } = config
-
   const feedMap: Record<string, Set<number>> = {}
+  const feeder = new RssFeedEmitter({ skipFirstLoad: true })
+
+  function subscribe(url: string, groupId: number) {
+    if (url in feedMap) {
+      feedMap[url].add(groupId)
+    } else {
+      feedMap[url] = new Set()
+      feeder.add({ url, refresh })
+    }
+  }
+
+  function unsubscribe(url: string, groupId: number) {
+    feedMap[url].delete(groupId)
+    if (!feedMap[url].size) {
+      delete feedMap[url]
+      feeder.remove(url)
+    }
+  }
 
   ctx.on('connect', async () => {
-    const feeder = new RssFeedEmitter({ skipFirstLoad: true })
-
     feeder.on('error', (err: Error) => {
-      // TODO remove subscription if returns 404?
       logger.warn(err)
     })
 
     const groups = await ctx.database.getAllGroups(['id', 'rss'])
     for (const group of groups) {
       for (const url of group.rss) {
-        if (url in feedMap) {
-          feedMap[url].add(group.id)
-        } else {
-          feedMap[url] = new Set()
-          feeder.add({ url, refresh })
-        }
+        subscribe(url, group.id)
       }
     }
 
@@ -45,32 +54,27 @@ export function apply(ctx: Context, config: Config = {}) {
       const source = payload.meta.link.toLowerCase()
       if (!feedMap[source]) return
       const message = `${payload.meta.title} (${payload.author})\n${payload.title}`
-      const groups = await ctx.database.getAllGroups(['id', 'assignee', 'flag'])
-      const groupMap = Object.fromEntries(groups.map(g => [g.id, g]))
-      for (const id of feedMap[source]) {
-        if (!groupMap[id].assignee || groupMap[id].flag & Group.Flag.noEmit) continue
-        ctx.bots[groupMap[id].assignee].sendGroupMsg(id, message)
-      }
+      await ctx.broadcast([...feedMap[source]], message)
     })
   })
 
   ctx.group().command('rss <url>', 'Subscribe a rss url')
-    .groupFields(['rss'])
+    .groupFields(['rss', 'id'])
     .option('remove', '-r, --remove 取消订阅')
-    .action(async ({ session, options }, url) => {
+    .action(async ({ session: { $group }, options }, url) => {
       url = url.toLowerCase()
 
-      const index = session.$group.rss.indexOf(url)
+      const index = $group.rss.indexOf(url)
       if (!options.remove) {
         if (index < 0) return '未订阅此链接。'
-        session.$group.rss.splice(index, 1)
-        feedMap[url].delete(session.groupId)
+        $group.rss.splice(index, 1)
+        unsubscribe(url, $group.id)
         return '取消订阅成功！'
       }
 
       if (index >= 0) return '已订阅此链接。'
-      session.$group.rss.push(url)
-      feedMap[url].add(session.groupId)
+      $group.rss.push(url)
+      subscribe(url, $group.id)
       return '添加订阅成功！'
     })
 }
