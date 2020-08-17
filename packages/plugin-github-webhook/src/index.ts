7b85ee6eeacd1106edb6bdf59f5d4dcc979dc53d
Shigma <1700011071@pku.edu.cn>
2020-08-17 23:25:09 +0800

    github-webhook: optimize output

--- packages/plugin-github-webhook/src/index.ts
+++ packages/plugin-github-webhook/src/index.ts
@@ -5,6 +5,8 @@ import { Webhooks, EventNames } from '@octokit/webhooks'
 import { Options, WebhookEvent } from '@octokit/webhooks/dist-types/types'
 import { GetWebhookPayloadTypeFromEvent } from '@octokit/webhooks/dist-types/generated/get-webhook-payload-type-from-event'
 
+type Payload<T extends EventNames.All> = GetWebhookPayloadTypeFromEvent<T, unknown>['payload']
+
 export interface Config extends Options<WebhookEvent> {
   repos?: Record<string, number[]>
 }
@@ -27,7 +29,7 @@ export function apply(ctx: Context, config: Config = {}) {
     return webhook.middleware(ctx.req, ctx.res, next)
   })
 
-  function registerHandler<T extends EventNames.All>(event: T, handler: (payload: GetWebhookPayloadTypeFromEvent<T>['payload']) => string | Promise<string>) {
+  function registerHandler<T extends EventNames.All>(event: T, handler: (payload: Payload<T>) => string | Promise<string>) {
     webhook.on(event, async (callback) => {
       const { repository } = callback.payload
       const ids = config.repos[repository.full_name]
@@ -40,8 +42,7 @@ export function apply(ctx: Context, config: Config = {}) {
 
   registerHandler('commit_comment.created', ({ repository, comment }) => {
     return [
-      `[GitHub] Commit Comment (${repository.full_name})`,
-      `User: ${comment.user.login}`,
+      `[GitHub] ${comment.user.login} commented on commit ${repository.full_name}@${comment.commit_id.slice(0, 6)}`,
       `URL: ${comment.html_url}`,
       comment.body.replace(/\n\s*\n/g, '\n'),
     ].join('\n')
@@ -49,18 +50,17 @@ export function apply(ctx: Context, config: Config = {}) {
 
   registerHandler('issues.opened', ({ repository, issue }) => {
     return [
-      `[GitHub] Issue Opened (${repository.full_name}#${issue.number})`,
+      `[GitHub] ${issue.user.login} opened an issue ${repository.full_name}#${issue.number}`,
       `Title: ${issue.title}`,
-      `User: ${issue.user.login}`,
       `URL: ${issue.html_url}`,
       issue.body.replace(/\n\s*\n/g, '\n'),
     ].join('\n')
   })
 
   registerHandler('issue_comment.created', ({ comment, issue, repository }) => {
+    const type = issue['pull_request'] ? 'pull request' : 'issue'
     return [
-      `[GitHub] ${issue['pull_request'] ? 'Pull Request' : 'Issue'} Comment (${repository.full_name}#${issue.number})`,
-      `User: ${comment.user.login}`,
+      `[GitHub] ${comment.user.login} commented on ${type} ${repository.full_name}#${issue.id}`,
       `URL: ${comment.html_url}`,
       comment.body.replace(/\n\s*\n/g, '\n'),
     ].join('\n')
@@ -68,9 +68,7 @@ export function apply(ctx: Context, config: Config = {}) {
 
   registerHandler('pull_request.opened', ({ repository, pull_request }) => {
     return [
-      `[GitHub] Pull Request Opened (${repository.full_name}#${pull_request.id})`,
-      `${pull_request.base.label} <- ${pull_request.head.label}`,
-      `User: ${pull_request.user.login}`,
+      `[GitHub] ${pull_request.user.login} opened a pull request ${repository.full_name}#${pull_request.id} (${pull_request.base.label} <- ${pull_request.head.label})`,
       `URL: ${pull_request.html_url}`,
       pull_request.body.replace(/\n\s*\n/g, '\n'),
     ].join('\n')
@@ -79,8 +77,7 @@ export function apply(ctx: Context, config: Config = {}) {
   registerHandler('pull_request_review.submitted', ({ repository, review, pull_request }) => {
     if (!review.body) return
     return [
-      `[GitHub] Pull Request Review (${repository.full_name}#${pull_request.id})`,
-      `User: ${review.user.login}`,
+      `[GitHub] ${review.user.login} reviewed pull request ${repository.full_name}#${pull_request.id}`,
       `URL: ${review.html_url}`,
       // @ts-ignore
       review.body.replace(/\n\s*\n/g, '\n'),
@@ -89,9 +86,8 @@ export function apply(ctx: Context, config: Config = {}) {
 
   registerHandler('pull_request_review_comment.created', ({ repository, comment, pull_request }) => {
     return [
-      `[GitHub] Pull Request Review Comment (${repository.full_name}#${pull_request.id})`,
+      `[GitHub] ${comment.user.login} commented on pull request review ${repository.full_name}#${pull_request.id}`,
       `Path: ${comment.path}`,
-      `User: ${comment.user.login}`,
       `URL: ${comment.html_url}`,
       comment.body.replace(/\n\s*\n/g, '\n'),
     ].join('\n')
@@ -107,9 +103,7 @@ export function apply(ctx: Context, config: Config = {}) {
     }
 
     return [
-      `[GitHub] Push (${repository.full_name})`,
-      `Ref: ${ref}`,
-      `User: ${pusher.name}`,
+      `[GitHub] ${pusher.name} pushed to ${repository.full_name} (${ref})`,
       `Compare: ${compare}`,
       ...commits.map(c => c.message.replace(/\n\s*\n/g, '\n')),
     ].join('\n')
