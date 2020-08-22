df43bf79f8cb94d509c2dea3a2e603e6ad3ef4e2
Shigma <1700011071@pku.edu.cn>
2020-08-23 02:50:46 +0800

    github: initialize interactions

--- packages/plugin-github/src/index.ts
+++ packages/plugin-github/src/index.ts
@@ -1,11 +1,19 @@
 /* eslint-disable camelcase */
 
-import { Context, User } from 'koishi-core'
-import { Webhooks, EventNames } from '@octokit/webhooks'
+import { Context, Middleware, User } from 'koishi-core'
+import { Time } from 'koishi-utils'
+import { Octokit } from '@octokit/rest'
+import { Webhooks, EventNames, EventPayloads } from '@octokit/webhooks'
 import { GetWebhookPayloadTypeFromEvent } from '@octokit/webhooks/dist-types/generated/get-webhook-payload-type-from-event'
+import { Agent } from 'http'
 import axios from 'axios'
 
 type Payload<T extends EventNames.All> = GetWebhookPayloadTypeFromEvent<T, unknown>['payload']
+type Repository = EventPayloads.PayloadRepository
+type Issue = EventPayloads.WebhookPayloadIssuesIssue
+  | EventPayloads.WebhookPayloadPullRequestPullRequest
+  | EventPayloads.WebhookPayloadPullRequestReviewPullRequest
+type ReviewComment = EventPayloads.WebhookPayloadPullRequestReviewCommentComment
 
 declare module 'koishi-core/dist/database' {
   interface User {
@@ -18,12 +26,15 @@ User.extend(() => ({
 }))
 
 export interface Config {
+  agent?: Agent
   secret?: string
   webhook?: string
   authorize?: string
-  clientId?: string
-  clientSecret?: string
+  appId?: string
+  appSecret?: string
   redirectUri?: string
+  replyTimeout?: number
+  requestTimeout?: number
   repos?: Record<string, number[]>
 }
 
@@ -31,6 +42,7 @@ const defaultOptions: Config = {
   secret: '',
   webhook: '/github/webhook',
   authorize: '/github/authorize',
+  replyTimeout: Time.hour,
   repos: {},
 }
 
@@ -41,6 +53,12 @@ export function apply(ctx: Context, config: Config = {}) {
 
   config = { ...defaultOptions, ...config }
   const webhook = new Webhooks(config)
+  const github = new Octokit({
+    request: {
+      agent: config.agent,
+      timeout: config.requestTimeout,
+    },
+  })
 
   const { router, database } = ctx
 
@@ -53,12 +71,12 @@ export function apply(ctx: Context, config: Config = {}) {
     if (Number.isNaN(targetId)) throw new Error('Invalid targetId')
     const { code, state } = ctx.query
     const { data } = await axios.post('https://github.com/login/oauth/access_token', {
-      client_id: config.clientId,
-      client_secret: config.clientSecret,
+      client_id: config.appId,
+      client_secret: config.appSecret,
       redirect_uri: config.redirectUri,
       code,
       state,
-    })
+    }, { httpsAgent: config.agent })
     if (data.access_token) {
       await database.setUser(targetId, { githubToken: data.access_token })
       return ctx.status = 200
@@ -67,14 +85,34 @@ export function apply(ctx: Context, config: Config = {}) {
     }
   })
 
-  function registerHandler<T extends EventNames.All>(event: T, handler: (payload: Payload<T>) => string) {
+  const interactions: Record<number, Middleware> = {}
+
+  ctx.middleware((session, next) => {
+    const middleware = interactions[session.$reply] || ((_, fallback) => next(fallback))
+    return middleware(session, next)
+  })
+
+  function registerHandler<T extends EventNames.All>(event: T, handler: (payload: Payload<T>) => [string, Middleware?]) {
     webhook.on(event, async (callback) => {
       const { repository } = callback.payload
-      const ids = config.repos[repository.full_name]
-      if (!ids) return
+      const groupIds = config.repos[repository.full_name]
+      if (!groupIds) return
 
-      const message = handler(callback.payload)
-      await ctx.broadcast(ids, message)
+      const result = handler(callback.payload)
+      if (!result) return
+
+      const [message, middleware] = result
+      const messageIds = await ctx.broadcast(groupIds, message)
+      if (!middleware) return
+
+      for (const id of messageIds) {
+        interactions[id] = middleware
+      }
+      setTimeout(() => {
+        for (const id of messageIds) {
+          delete interactions[id]
+        }
+      }, config.replyTimeout)
     })
   }
 
@@ -85,56 +123,89 @@ export function apply(ctx: Context, config: Config = {}) {
   }
 
   registerHandler('commit_comment.created', ({ repository, comment }) => {
-    return [
+    return [[
       `[GitHub] ${comment.user.login} commented on commit ${repository.full_name}@${comment.commit_id.slice(0, 6)}`,
       `URL: ${comment.html_url}`,
       formatMarkdown(comment.body),
-    ].join('\n')
+    ].join('\n')]
   })
 
+  const createIssueComment = (repo: Repository, issue: Issue): Middleware => async (session, next) => {
+    const body = session.$parsed
+    if (!body) return next()
+    const user = await session.$observeUser(['githubToken'])
+    if (!user.githubToken) return // TODO hint
+    await github.issues.createComment({
+      owner: repo.owner.login,
+      repo: repo.name,
+      issue_number: issue.number,
+      body,
+      headers: {
+        Authorization: `token ${user.githubToken}`,
+      },
+    })
+  }
+
+  const createReviewCommentReply = (repo: Repository, issue: Issue, comment: ReviewComment): Middleware => async (session, next) => {
+    const body = session.$parsed
+    if (!body) return next()
+    const user = await session.$observeUser(['githubToken'])
+    if (!user.githubToken) return // TODO hint
+    await github.pulls.createReplyForReviewComment({
+      owner: repo.owner.login,
+      repo: repo.name,
+      pull_number: issue.number,
+      comment_id: comment.id,
+      body,
+      headers: {
+        Authorization: `token ${user.githubToken}`,
+      },
+    })
+  }
+
   registerHandler('issues.opened', ({ repository, issue }) => {
-    return [
+    return [[
       `[GitHub] ${issue.user.login} opened an issue ${repository.full_name}#${issue.number}`,
       `URL: ${issue.html_url}`,
       `Title: ${issue.title}`,
       formatMarkdown(issue.body),
-    ].join('\n')
+    ].join('\n'), createIssueComment(repository, issue)]
   })
 
   registerHandler('issue_comment.created', ({ comment, issue, repository }) => {
     const type = issue['pull_request'] ? 'pull request' : 'issue'
-    return [
+    return [[
       `[GitHub] ${comment.user.login} commented on ${type} ${repository.full_name}#${issue.number}`,
       `URL: ${comment.html_url}`,
       formatMarkdown(comment.body),
-    ].join('\n')
+    ].join('\n'), createIssueComment(repository, issue)]
   })
 
   registerHandler('pull_request.opened', ({ repository, pull_request }) => {
-    return [
+    return [[
       `[GitHub] ${pull_request.user.login} opened a pull request ${repository.full_name}#${pull_request.number} (${pull_request.base.label} <- ${pull_request.head.label})`,
       `URL: ${pull_request.html_url}`,
       formatMarkdown(pull_request.body),
-    ].join('\n')
+    ].join('\n'), createIssueComment(repository, pull_request)]
   })
 
   registerHandler('pull_request_review.submitted', ({ repository, review, pull_request }) => {
     if (!review.body) return
-    return [
+    return [[
       `[GitHub] ${review.user.login} reviewed pull request ${repository.full_name}#${pull_request.number}`,
       `URL: ${review.html_url}`,
       // @ts-ignore
       formatMarkdown(review.body),
-    ].join('\n')
+    ].join('\n'), createIssueComment(repository, pull_request)]
   })
 
   registerHandler('pull_request_review_comment.created', ({ repository, comment, pull_request }) => {
-    return [
+    return [[
       `[GitHub] ${comment.user.login} commented on pull request review ${repository.full_name}#${pull_request.number}`,
       `Path: ${comment.path}`,
       `URL: ${comment.html_url}`,
       formatMarkdown(comment.body),
-    ].join('\n')
+    ].join('\n'), createReviewCommentReply(repository, pull_request, comment)]
   })
 
   registerHandler('push', ({ compare, pusher, commits, repository, ref, after }) => {
@@ -143,13 +214,13 @@ export function apply(ctx: Context, config: Config = {}) {
 
     // use short form for tag releases
     if (ref.startsWith('refs/tags')) {
-      return `[GitHub] ${pusher.name} published tag ${repository.full_name}@${ref.slice(10)}`
+      return [`[GitHub] ${pusher.name} published tag ${repository.full_name}@${ref.slice(10)}`]
     }
 
-    return [
+    return [[
       `[GitHub] ${pusher.name} pushed to ${repository.full_name}:${ref}`,
       `Compare: ${compare}`,
       ...commits.map(c => `[${c.id.slice(0, 6)}] ${formatMarkdown(c.message)}`),
-    ].join('\n')
+    ].join('\n')]
   })
 }
