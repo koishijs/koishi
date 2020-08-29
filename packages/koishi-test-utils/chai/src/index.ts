caa2c7521fb30ae2a6786d60e50148f315ff26ef
Shigma <1700011071@pku.edu.cn>
2020-08-30 00:40:05 +0800

    test-utils: chai extension test

--- packages/koishi-test-utils/chai/src/index.ts
+++ packages/koishi-test-utils/chai/src/index.ts
@@ -1,41 +1,43 @@
 import { use } from 'chai'
+import { inspect } from 'util'
 
 use(({ Assertion }) => {
-  function sameShape(expect, actual, path) {
+  function checkShape(expect, actual, path) {
     if (actual === expect || Number.isNaN(expect) && Number.isNaN(actual)) return
 
+    function formatError(expect, actual) {
+      return `expected to have ${expect} but got ${actual} at path ${path}`
+    }
+
     if (!expect || ['string', 'number', 'boolean', 'bigint'].includes(typeof expect)) {
-      return 'Expected to have null but got "' + actual + '" at path "' + path + '".'
+      return formatError(inspect(expect), inspect(actual))
     }
 
     // dates
     if (expect instanceof Date) {
-      if (!(actual instanceof Date)) {
-        return 'Expected to have date "' + expect.toISOString() + '" but got '
-          + '"' + actual + '" at path "' + path + '".'
-      } else if (expect.getTime() !== actual.getTime()) {
-        return 'Expected to have date "' + expect.toISOString() + '" but got ' +
-          '"' + actual.toISOString() + '" at path "' + path + '".'
+      if (!(actual instanceof Date) || +expect !== +actual) {
+        return formatError(inspect(expect), inspect(actual))
       }
       return
     }
 
     if (actual === null) {
-      return 'Expected to have an array/object but got null at path "' + path + '".'
+      const type = Object.prototype.toString.call(expect).slice(8, -1).toLowerCase()
+      return formatError(`a ${type}`, 'null')
     }
 
-    // array/object description
+    // array / object
     for (const prop in expect) {
       if (typeof actual[prop] === 'undefined' && typeof expect[prop] !== 'undefined') {
-        return 'Expected "' + prop + '" field to be defined at path "' + path + '".'
+        return `expected "${prop}" field to be defined at path ${path}`
       }
-      const message = sameShape(expect[prop], actual[prop], path + (path === '/' ? '' : '/') + prop)
+      const message = checkShape(expect[prop], actual[prop], `${path}${prop}/`)
       if (message) return message
     }
   }
 
   Assertion.addMethod('shape', function (expect) {
-    const message = sameShape(expect, this._obj, '/')
+    const message = checkShape(expect, this._obj, '/')
     if (message) this.assert(false, message, undefined, expect, this._obj)
   })
 })
