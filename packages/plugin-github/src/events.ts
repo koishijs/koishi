b9c0f31086e58fa1020251bb2ff268304369c713
Shigma <1700011071@pku.edu.cn>
2020-09-01 16:42:55 +0800

    feat(github): support refresh token

--- packages/plugin-github/src/events.ts
+++ packages/plugin-github/src/events.ts
@@ -1,128 +1,24 @@
 /* eslint-disable camelcase */
-/* eslint-disable quote-props */
 
-import { EventNames, Webhooks } from '@octokit/webhooks'
+import { EventNames } from '@octokit/webhooks'
 import { GetWebhookPayloadTypeFromEvent } from '@octokit/webhooks/dist-types/generated/get-webhook-payload-type-from-event'
-import { Context, Session } from 'koishi-core'
-import { defineProperty } from 'koishi-utils'
-import { Config } from '.'
-import axios from 'axios'
 
-interface ReplyPayloads {
+export interface ReplyPayloads {
   link?: string
   reply?: [url: string, params?: Record<string, any>],
 }
 
 type Payload<T extends EventNames.All> = GetWebhookPayloadTypeFromEvent<T, unknown>['payload']
 type EventHandler<T extends EventNames.All> = (payload: Payload<T>) => [message: string, replies?: ReplyPayloads]
-type ReplyHandlers = {
-  [K in keyof ReplyPayloads]: (payload: ReplyPayloads[K], session: Session, message: string) => Promise<void>
-}
 
-export default function apply(ctx: Context, config: Config) {
-  const webhooks = new Webhooks({
-    ...config,
-    path: config.webhook,
-  })
-
-  defineProperty(ctx.app, 'githubWebhooks', webhooks)
-
-  interface RestOptions {
-    url: string
-    session: Session<'githubToken'>
-    message: string
-    params: Record<string, any>
-  }
-
-  const request = async ({ session, url, message, params }: RestOptions) => {
-    if (!session.$user.githubToken) {
-      await session.$send('如果想使用此功能，请对机器人进行授权，输入你的 GitHub 用户名。')
-      const name = await session.$prompt().catch<string>()
-      if (!name) return
-      return session.$execute({ command: 'github', args: [name] })
-    }
-
-    await axios.post(url, { ...params, body: message }, {
-      httpsAgent: config.agent,
-      timeout: config.requestTimeout,
-      headers: {
-        'User-Agent': 'koishi-plugin-github',
-        'Authorization': `token ${session.$user.githubToken}`,
-      },
-    })
-  }
-
-  const replyHandlers: ReplyHandlers = {
-    link: (url, session) => session.$send(url),
-    reply: ([url, params], session, message) => request({ url, session, message, params }),
-  }
-
-  const interactions: Record<number, ReplyPayloads> = {}
-
-  ctx.router.post(config.webhook, (ctx, next) => {
-    // workaround @octokit/webhooks for koa
-    ctx.req['body'] = ctx.request.body
-    ctx.status = 200
-    return webhooks.middleware(ctx.req, ctx.res, next)
-  })
-
-  ctx.on('before-attach-user', (session, fields) => {
-    if (interactions[session.$reply]) {
-      fields.add('githubToken')
-    }
-  })
-
-  ctx.middleware((session, next) => {
-    const body = session.$parsed
-    const payloads = interactions[session.$reply]
-    if (!body || !payloads) return next()
-
-    let name: string, message: string
-    if (body.startsWith('.')) {
-      name = body.split(' ', 1)[0].slice(1)
-      message = body.slice(2 + name.length).trim()
-    } else {
-      // fallback to reply
-      name = 'reply'
-      message = body
-    }
-
-    const payload = payloads[name]
-    if (!payload) return next()
-    return replyHandlers[name](payload, session, message)
-  })
-
-  function registerHandler<T extends EventNames.All>(event: T, handler: EventHandler<T>) {
-    webhooks.on(event, async (callback) => {
-      const { repository } = callback.payload
-      const groupIds = config.repos[repository.full_name]
-      if (!groupIds) return
-
-      const result = handler(callback.payload)
-      if (!result) return
-
-      const [message, replies] = result
-      const messageIds = await ctx.broadcast(groupIds, message)
-      if (!replies) return
-
-      for (const id of messageIds) {
-        interactions[id] = replies
-      }
-      setTimeout(() => {
-        for (const id of messageIds) {
-          delete interactions[id]
-        }
-      }, config.replyTimeout)
-    })
-  }
-
+export function addListeners(on: <T extends EventNames.All>(event: T, handler: EventHandler<T>) => void) {
   function formatMarkdown(source: string) {
     return source
       .replace(/^```(.*)$/gm, '')
       .replace(/\n\s*\n/g, '\n')
   }
 
-  registerHandler('commit_comment.created', ({ repository, comment }) => {
+  on('commit_comment.created', ({ repository, comment }) => {
     const { full_name } = repository
     const { user, html_url, commit_id, body, path, position } = comment
     if (user.type === 'bot') return
@@ -137,12 +33,12 @@ export default function apply(ctx: Context, config: Config) {
     }]
   })
 
-  registerHandler('fork', ({ repository, sender, forkee }) => {
+  on('fork', ({ repository, sender, forkee }) => {
     const { full_name, forks_count } = repository
     return [`[GitHub] ${sender.login} forked ${full_name} to ${forkee.full_name} (total ${forks_count} forks)`]
   })
 
-  registerHandler('issues.opened', ({ repository, issue }) => {
+  on('issues.opened', ({ repository, issue }) => {
     const { full_name } = repository
     const { user, html_url, comments_url, title, body, number } = issue
     if (user.type === 'bot') return
@@ -154,7 +50,7 @@ export default function apply(ctx: Context, config: Config) {
     ].join('\n'), { link: html_url, reply: [comments_url] }]
   })
 
-  registerHandler('issue_comment.created', ({ comment, issue, repository }) => {
+  on('issue_comment.created', ({ comment, issue, repository }) => {
     const { full_name } = repository
     const { number, comments_url } = issue
     const { user, html_url, body } = comment
@@ -167,7 +63,7 @@ export default function apply(ctx: Context, config: Config) {
     ].join('\n'), { link: html_url, reply: [comments_url] }]
   })
 
-  registerHandler('pull_request.opened', ({ repository, pull_request }) => {
+  on('pull_request.opened', ({ repository, pull_request }) => {
     const { full_name } = repository
     const { user, html_url, comments_url, base, head, body, number } = pull_request
     if (user.type === 'bot') return
@@ -178,7 +74,7 @@ export default function apply(ctx: Context, config: Config) {
     ].join('\n'), { link: html_url, reply: [comments_url] }]
   })
 
-  registerHandler('pull_request_review.submitted', ({ repository, review, pull_request }) => {
+  on('pull_request_review.submitted', ({ repository, review, pull_request }) => {
     if (!review.body) return
     const { full_name } = repository
     const { number, comments_url } = pull_request
@@ -191,7 +87,7 @@ export default function apply(ctx: Context, config: Config) {
     ].join('\n'), { link: html_url, reply: [comments_url] }]
   })
 
-  registerHandler('pull_request_review_comment.created', ({ repository, comment, pull_request }) => {
+  on('pull_request_review_comment.created', ({ repository, comment, pull_request }) => {
     const { full_name } = repository
     const { number } = pull_request
     const { user, path, body, html_url, url } = comment
@@ -203,7 +99,7 @@ export default function apply(ctx: Context, config: Config) {
     ].join('\n'), { link: html_url, reply: [url] }]
   })
 
-  registerHandler('push', ({ compare, pusher, commits, repository, ref, after }) => {
+  on('push', ({ compare, pusher, commits, repository, ref, after }) => {
     const { full_name } = repository
 
     // do not show pull request merge
@@ -220,7 +116,7 @@ export default function apply(ctx: Context, config: Config) {
     ].join('\n'), { link: compare }]
   })
 
-  registerHandler('star.created', ({ repository, sender }) => {
+  on('star.created', ({ repository, sender }) => {
     const { full_name, stargazers_count } = repository
     return [`[GitHub] ${sender.login} starred ${full_name} (total ${stargazers_count} stargazers)`]
   })
--- packages/plugin-github/src/index.ts
+++ packages/plugin-github/src/index.ts
@@ -1,12 +1,13 @@
 /* eslint-disable camelcase */
+/* eslint-disable quote-props */
 
-import { Context, User } from 'koishi-core'
-import { Logger, Time } from 'koishi-utils'
+import { Context, Session, User } from 'koishi-core'
+import { defineProperty, Logger, Time } from 'koishi-utils'
 import { Webhooks } from '@octokit/webhooks'
 import { Agent } from 'https'
 import { encode } from 'querystring'
-import axios from 'axios'
-import events from './events'
+import axios, { AxiosError } from 'axios'
+import { addListeners, ReplyPayloads } from './events'
 
 declare module 'koishi-core/dist/app' {
   interface App {
@@ -16,20 +17,34 @@ declare module 'koishi-core/dist/app' {
 
 declare module 'koishi-core/dist/database' {
   interface User {
-    githubToken?: string
+    ghAccessToken?: string
+    ghRefreshToken?: string
   }
 }
 
 User.extend(() => ({
-  githubToken: '',
+  ghAccessToken: '',
+  ghRefreshToken: '',
 }))
 
-const logger = new Logger('github')
-
-interface AuthorizeResult {
+export interface OAuth {
   access_token: string
+  expires_in: string
+  refresh_token: string
+  refresh_token_expires_in: string
   token_type: string
   scope: string
+}
+
+interface RequestOptions {
+  url: string
+  session: Session<'ghAccessToken' | 'ghRefreshToken'>
+  message: string
+  params: Record<string, any>
+}
+
+type ReplyHandlers = {
+  [K in keyof ReplyPayloads]: (payload: ReplyPayloads[K], session: Session, message: string) => Promise<void>
 }
 
 export interface Config {
@@ -54,35 +69,43 @@ const defaultOptions: Config = {
   repos: {},
 }
 
+const logger = new Logger('github')
+
 export const name = 'github'
 
 export function apply(ctx: Context, config: Config = {}) {
   config = { ...defaultOptions, ...config }
-  ctx.plugin(events, config)
 
-  const { database } = ctx
+  const webhooks = new Webhooks({
+    ...config,
+    path: config.webhook,
+  })
 
-  ctx.router.get(config.authorize, async (ctx) => {
-    const targetId = parseInt(ctx.query.state)
-    if (Number.isNaN(targetId)) throw new Error('Invalid targetId')
-    const { code, state } = ctx.query
-    const { data } = await axios.post<AuthorizeResult>('https://github.com/login/oauth/access_token', {
+  const { app, database, router } = ctx
+  defineProperty(app, 'githubWebhooks', webhooks)
+
+  async function getTokens(params: any) {
+    const { data } = await axios.post<OAuth>('https://github.com/login/oauth/access_token', {
       client_id: config.appId,
       client_secret: config.appSecret,
-      redirect_uri: config.redirect,
-      code,
-      state,
+      ...params,
     }, {
       httpsAgent: config.agent,
       headers: { Accept: 'application/json' },
     })
-    if (data.access_token) {
-      await database.setUser(targetId, { githubToken: data.access_token })
-      return ctx.status = 200
-    } else {
-      logger.warn(data)
-      return ctx.status = 500
-    }
+    return data
+  }
+
+  router.get(config.authorize, async (ctx) => {
+    const targetId = parseInt(ctx.query.state)
+    if (Number.isNaN(targetId)) throw new Error('Invalid targetId')
+    const { code, state } = ctx.query
+    const data = await getTokens({ code, state, redirect_uri: config.redirect })
+    await database.setUser(targetId, {
+      ghAccessToken: data.access_token,
+      ghRefreshToken: data.refresh_token,
+    })
+    return ctx.status = 200
   })
 
   ctx.command('github <user>', '授权 GitHub 功能')
@@ -97,4 +120,122 @@ export function apply(ctx: Context, config: Config = {}) {
       })
       return '请点击下面的链接继续操作：\n' + url
     })
+
+  async function plainRequest({ session, url, message, params }: RequestOptions) {
+    await axios.post(url, { ...params, body: message }, {
+      httpsAgent: config.agent,
+      timeout: config.requestTimeout,
+      headers: {
+        'Accept': 'application/vnd.github.v3+json',
+        'Authorization': `token ${session.$user.ghAccessToken}`,
+      },
+    })
+  }
+
+  async function authorize(session: Session, message: string) {
+    await session.$send(message)
+    const name = await session.$prompt().catch<string>()
+    if (!name) return
+    return session.$execute({ command: 'github', args: [name] })
+  }
+
+  async function request(options: RequestOptions) {
+    const { session } = options
+    if (!session.$user.ghAccessToken) {
+      return authorize(session, '如果想使用此功能，请对机器人进行授权。输入你的 GitHub 用户名。')
+    }
+
+    try {
+      return await plainRequest(options)
+    } catch (error) {
+      const { response } = error as AxiosError
+      if (response?.status !== 401) {
+        logger.warn(error)
+        return session.$send('发送失败。')
+      }
+    }
+
+    try {
+      const data = await getTokens({
+        refresh_token: session.$user.ghRefreshToken,
+        grant_type: 'refresh_token',
+      })
+      session.$user.ghAccessToken = data.access_token
+      session.$user.ghRefreshToken = data.refresh_token
+    } catch (error) {
+      return authorize(session, '令牌已失效，需要重新授权。输入你的 GitHub 用户名。')
+    }
+
+    try {
+      await plainRequest(options)
+    } catch (error) {
+      logger.warn(error)
+      return session.$send('发送失败。')
+    }
+  }
+
+  const replyHandlers: ReplyHandlers = {
+    link: (url, session) => session.$send(url),
+    reply: ([url, params], session, message) => request({ url, session, message, params }),
+  }
+
+  const interactions: Record<number, ReplyPayloads> = {}
+
+  router.post(config.webhook, (ctx, next) => {
+    // workaround @octokit/webhooks for koa
+    ctx.req['body'] = ctx.request.body
+    ctx.status = 200
+    return webhooks.middleware(ctx.req, ctx.res, next)
+  })
+
+  ctx.on('before-attach-user', (session, fields) => {
+    if (interactions[session.$reply]) {
+      fields.add('ghAccessToken')
+      fields.add('ghRefreshToken')
+    }
+  })
+
+  ctx.middleware((session, next) => {
+    const body = session.$parsed
+    const payloads = interactions[session.$reply]
+    if (!body || !payloads) return next()
+
+    let name: string, message: string
+    if (body.startsWith('.')) {
+      name = body.split(' ', 1)[0].slice(1)
+      message = body.slice(2 + name.length).trim()
+    } else {
+      // fallback to reply
+      name = 'reply'
+      message = body
+    }
+
+    const payload = payloads[name]
+    if (!payload) return next()
+    return replyHandlers[name](payload, session, message)
+  })
+
+  addListeners((event, handler) => {
+    webhooks.on(event, async (callback) => {
+      const { repository } = callback.payload
+      const groupIds = config.repos[repository.full_name]
+      if (!groupIds) return
+
+      const result = handler(callback.payload)
+      if (!result) return
+
+      const [message, replies] = result
+      const messageIds = await ctx.broadcast(groupIds, message)
+      if (!replies) return
+
+      for (const id of messageIds) {
+        interactions[id] = replies
+      }
+      setTimeout(() => {
+        for (const id of messageIds) {
+          delete interactions[id]
+        }
+      }, config.replyTimeout)
+    })
+  })
 }
