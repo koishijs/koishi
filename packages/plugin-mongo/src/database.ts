b8d28d3b3ea0c376db9794c9e85029a4e3b8c2d6
undefined <masnn0@outlook.com>
2020-10-10 12:05:06 +0800

    Update database.ts

--- packages/plugin-mongo/src/database.ts
+++ packages/plugin-mongo/src/database.ts
@@ -11,17 +11,13 @@ export interface Config {
   prefix?: string
 }
 
-interface Document {
-  _id: number
-}
-
 export default class MongoDatabase {
   public config: Config
   public client: MongoClient
   public db: Db
 
-  user: Collection<User & Document>
-  group: Collection<Group & Document>
+  user: Collection<any>
+  group: Collection<any>
 
   constructor(public app: App, config: Config) {
     this.config = config
