ed22675d928ecf04db50f177b114ef4c5c93a0d4
arily <lbsaaaaa@gmail.com>
2020-10-21 15:10:13 +0900

    Update database.ts

--- packages/plugin-mongo/src/database.ts
+++ packages/plugin-mongo/src/database.ts
@@ -11,7 +11,7 @@ export interface Config {
   name?: string // database name
   prefix?: string
   authDatabase?: string // default auth database
-  connectionOptions?: string | { [key: string]: string | number | boolean | (string | number)[] } | [string, string][]
+  connectionOptions?: URLSearchParams | string | object | Iterable<[string, string]> | Array<[string, string]>
   /** connection string (will overwrite all configs except 'name' and 'prefix') */
   uri?: string
 }
