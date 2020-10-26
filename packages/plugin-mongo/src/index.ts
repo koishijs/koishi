8d218f84e37b450ac0da1ebd5d0941e41ed17fda
undefined <masnn0@outlook.com>
2020-10-26 13:29:39 +0800

    1

--- packages/plugin-mongo/src/index.ts
+++ packages/plugin-mongo/src/index.ts
@@ -8,6 +8,55 @@ declare module 'koishi-core/dist/database' {
   interface Database extends MongoDatabase { }
 }
 
+function escapeKey<T extends Partial<User>>(doc: T) {
+  const data: T = { ...doc }
+  delete data.timers
+  delete data.usage
+  if (doc.timers) {
+    data.timers = {}
+    for (const key in doc.timers) {
+      if (key === '$date') data.timers._date = doc.timers.$date
+      else data.timers[key.replace(/\./gmi, '_')] = doc.timers[key]
+    }
+  }
+  if (doc.usage) {
+    data.usage = {}
+    for (const key in doc.usage) {
+      if (key === '$date') data.usage._date = doc.usage.$date
+      else data.usage[key.replace(/\./gmi, '_')] = doc.usage[key]
+    }
+  }
+  return data
+}
+
+function unescapeKey<T extends Partial<User>>(data: T) {
+  if (data.timers) {
+    if (data.timers._date) {
+      data.timers.$date = data.timers._date
+      delete data.timers._date
+    }
+    for (const key in data.timers) {
+      if (key.includes('_')) {
+        data.timers[key.replace(/_/gmi, '.')] = data.timers[key]
+        delete data.timers[key]
+      }
+    }
+  }
+  if (data.usage) {
+    if (data.usage._date) {
+      data.usage.$date = data.usage._date
+      delete data.usage._date
+    }
+    for (const key in data.usage) {
+      if (key.includes('_')) {
+        data.usage[key.replace(/_/gmi, '.')] = data.usage[key]
+        delete data.usage[key]
+      }
+    }
+  }
+  return data
+}
+
 extendDatabase(MongoDatabase, {
   async getUser(userId, ...args) {
     const authority = typeof args[0] === 'number' ? args.shift() as number : 0
@@ -17,31 +66,7 @@ extendDatabase(MongoDatabase, {
     if (authority < 0) return null
     const fallback = User.create(userId, authority)
     if (authority && [undefined, null].includes(data.authority)) await this.user.updateOne({ _id: userId }, { $set: { authority } }, { upsert: true })
-    if (data.timers) {
-      if (data.timers._date) {
-        data.timers.$date = data.timers._date
-        delete data.timers._date
-      }
-      for (const key in data.timers) {
-        if (key.includes('_')) {
-          data.timers[key.replace(/_/gmi, '.')] = data.timers[key]
-          delete data.timers[key]
-        }
-      }
-    }
-    if (data.usage) {
-      if (data.usage._date) {
-        data.usage.$date = data.usage._date
-        delete data.usage._date
-      }
-      for (const key in data.usage) {
-        if (key.includes('_')) {
-          data.usage[key.replace(/_/gmi, '.')] = data.usage[key]
-          delete data.usage[key]
-        }
-      }
-    }
-    return { ...fallback, ...data }
+    return { ...fallback, ...unescapeKey(data) }
   },
 
   async getUsers(...args) {
@@ -57,54 +82,11 @@ extendDatabase(MongoDatabase, {
     if (ids && !ids.length) return []
     const f = {}
     for (const field of fields) f[field] = 1
-    return this.user.find({ _id: { $in: ids as number[] } }).project(f).map((doc) => {
-      if (doc.timers) {
-        if (doc.timers._date) {
-          doc.timers.$date = doc.timers._date
-          delete doc.timers._date
-        }
-        for (const key in doc.timers) {
-          if (key.includes('_')) {
-            doc.timers[key.replace(/_/gmi, '.')] = doc.timers[key]
-            delete doc.timers[key]
-          }
-        }
-      }
-      if (doc.usage) {
-        if (doc.usage._date) {
-          doc.usage.$date = doc.usage._date
-          delete doc.usage._date
-        }
-        for (const key in doc.usage) {
-          if (key.includes('_')) {
-            doc.usage[key.replace(/_/gmi, '.')] = doc.usage[key]
-            delete doc.usage[key]
-          }
-        }
-      }
-      return doc
-    }).toArray()
+    return this.user.find({ _id: { $in: ids as number[] } }).project(f).map(unescapeKey).toArray()
   },
 
   async setUser(userId, data) {
-    const $set: any = { ...data }
-    delete $set.timers
-    delete $set.usage
-    if (data.timers) {
-      $set.timers = {}
-      for (const key in data.timers) {
-        if (key === '$date') $set.timers._date = data.timers.$date
-        else $set.timers[key.replace(/\./gmi, '_')] = data.timers[key]
-      }
-    }
-    if (data.usage) {
-      $set.usage = {}
-      for (const key in data.usage) {
-        if (key === '$date') $set.usage._date = data.usage.$date
-        else $set.usage[key.replace(/\./gmi, '_')] = data.usage[key]
-      }
-    }
-    await this.user.updateOne({ _id: userId }, { $set }, { upsert: true })
+    await this.user.updateOne({ _id: userId }, { $set: escapeKey(data) }, { upsert: true })
   },
 
   async getGroup(groupId, ...args) {
@@ -116,7 +98,7 @@ extendDatabase(MongoDatabase, {
     const [data] = await this.group.find({ _id: groupId }).project(f).toArray()
     const fallback = Group.create(groupId, selfId)
     if (!data?.assignee && selfId && groupId) {
-        await this.group.updateOne({ _id: groupId }, { $set: { assignee: selfId, flag: 0 } }, { upsert: true })
+      await this.group.updateOne({ _id: groupId }, { $set: { assignee: selfId, flag: 0 } }, { upsert: true })
     }
     return { ...fallback, ...data }
   },
@@ -137,7 +119,11 @@ extendDatabase(MongoDatabase, {
     if (!assignees.length) return []
     const f = {}
     for (const field of fields) f[field] = 1
-    return this.group.find({ assignee: { $in: assignees } }).project(f).toArray()
+    const fallback = Group.create(0, 0)
+    const results = await this.group.find({ assignee: { $in: assignees } }).project(f).toArray()
+    return results.map(result => ({
+      ...fallback, ...result, id: result._id,
+    }))
   },
 
   async setGroup(groupId, data) {
