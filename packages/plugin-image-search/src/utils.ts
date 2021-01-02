8880720e17e69ed56a35accea769d98e78ac0d88
Shigma <1700011071@pku.edu.cn>
2021-01-02 11:39:44 +0800

    fix(image-search): use www.pixiv.net

--- packages/plugin-image-search/src/utils.ts
+++ packages/plugin-image-search/src/utils.ts
@@ -2,9 +2,9 @@ import { CQCode } from 'koishi-utils'
 
 export function getLink(url: string) {
   const pidSearch = /pixiv.+illust_id=(\d+)/.exec(url)
-  if (pidSearch) return 'https://pixiv.net/i/' + pidSearch[1]
+  if (pidSearch) return 'https://www.pixiv.net/i/' + pidSearch[1]
   const uidSearch = /pixiv.+member\.php\?id=(\d+)/.exec(url)
-  if (uidSearch) return 'https://pixiv.net/u/' + uidSearch[1]
+  if (uidSearch) return 'https://www.pixiv.net/u/' + uidSearch[1]
   return url
 }
 
