f287fc239dcb47973621d1fd8ad6df28c1a5b22f
Shigma <1700011071@pku.edu.cn>
2021-02-28 17:28:26 +0800

    test: setup eval tests

--- .mocharc.js
+++ .mocharc.js
@@ -27,26 +27,18 @@ function getSpecFromArgv() {
 
 module.exports = {
   exit: true,
-  extension: ['ts'],
   spec: getSpecFromArgv() || [
     // 'packages/koishi-core/tests/*.spec.ts',
     'packages/koishi-core/tests/context.spec.ts',
     'packages/koishi-core/tests/hook.spec.ts',
     'packages/koishi-core/tests/session.spec.ts',
     'packages/koishi-utils/tests/*.spec.ts',
-    // 'packages/koishi-test-utils/tests/*.spec.ts',
+    'packages/koishi-test-utils/tests/*.spec.ts',
     'packages/plugin-common/tests/admin.spec.ts',
     // 'packages/plugin-common/tests/handler.spec.ts',
     'packages/plugin-common/tests/sender.spec.ts',
-    // 'packages/plugin-eval/tests/*.spec.ts',
+    'packages/plugin-eval/tests/*.spec.ts',
     'packages/plugin-github/tests/*.spec.ts',
     'packages/plugin-teach/tests/*.spec.ts',
-  ],
-  require: process.env.USE_TS_NODE ? [
-    'ts-node/register/transpile-only',
-    'tsconfig-paths/register',
-    'build/setup-test',
-  ] : [
-    __dirname + '/build/register',
   ],
 }
