b42773b7e890e89de2c7c67bc70d070d3270df32
Shigma <1700011071@pku.edu.cn>
2021-03-09 15:27:53 +0800

    feat(teach): re-add substitute flag, fix #147

--- packages/plugin-teach/tests/writer.spec.ts
+++ packages/plugin-teach/tests/writer.spec.ts
@@ -1,11 +1,12 @@
 import createEnvironment from './environment'
 import jest from 'jest-mock'
 
-const DETAIL_HEAD = '编号为 1 的问答信息：\n问题：foo\n回答：bar\n'
+const DETAIL_HEAD_QES = '编号为 1 的问答信息：\n问题：foo\n'
+const DETAIL_HEAD = DETAIL_HEAD_QES + '回答：bar\n'
 const SEARCH_HEAD = '问题“foo”的回答如下：\n'
 
 describe('Teach Plugin - Writer', () => {
-  const { app, u2, u3g1, u4g2 } = createEnvironment({ useWriter: true })
+  const { app, u2g1, u3g1, u4g2 } = createEnvironment({ useWriter: true })
 
   app.command('test').action(({ session }) => '' + session.userId)
 
@@ -22,7 +23,7 @@ describe('Teach Plugin - Writer', () => {
   })
 
   it('modify writer', async () => {
-    await u2.shouldReply('#1 -W', '问答 1 因权限过低无法修改。')
+    await u2g1.shouldReply('#1 -W', '问答 1 因权限过低无法修改。')
     await u4g2.shouldReply('#1 -w foo', '选项 writer 输入无效，请指定正确的用户。')
     await u4g2.shouldReply('#1 -w [CQ:at,id=500]', '指定的目标用户不存在。')
     await u4g2.shouldReply('#1 -w [CQ:at,id=200]', '问答 1 已成功修改。')
@@ -36,11 +37,11 @@ describe('Teach Plugin - Writer', () => {
   })
 
   it('anonymous', async () => {
-    u2.meta.author.username = 'nick2'
-    await u2.shouldReply('#1', DETAIL_HEAD + '来源：nick2 (200)')
-    await u2.shouldReply('#1 -W', '问答 1 已成功修改。')
-    await u2.shouldReply('#1', DETAIL_HEAD.slice(0, -1))
-    await u2.shouldReply('#1 -p 0', '问答 1 因权限过低无法修改。')
+    u2g1.meta.author.username = 'nick2'
+    await u2g1.shouldReply('#1', DETAIL_HEAD + '来源：nick2 (200)')
+    await u2g1.shouldReply('#1 -W', '问答 1 已成功修改。')
+    await u2g1.shouldReply('#1', DETAIL_HEAD.slice(0, -1))
+    await u2g1.shouldReply('#1 -p 0', '问答 1 因权限过低无法修改。')
   })
 
   it('frozen', async () => {
@@ -50,5 +51,18 @@ describe('Teach Plugin - Writer', () => {
     await u3g1.shouldReply('#1', DETAIL_HEAD + '此问答已锁定。')
     await u3g1.shouldReply('## foo', SEARCH_HEAD + '1. [锁定] bar')
     await u4g2.shouldReply('#1 -F', '问答 1 已成功修改。')
+  })
+
+  it('substitute', async () => {
+    app.command('bar', { authority: 3 }).action(() => 'test')
+    await u3g1.shouldReply('#1 ~ $(bar) -w @300', '问答 1 已成功修改。')
+    const DETAIL_SPECIAL = DETAIL_HEAD_QES + '回答：$(bar)\n来源：user3 (300)'
+    await u3g1.shouldReply('#1', DETAIL_SPECIAL)
+    await u2g1.shouldReply('foo', '权限不足。')
+
+    await u3g1.shouldReply('#1 -s', '问答 1 已成功修改。')
+    await u3g1.shouldReply('#1', DETAIL_SPECIAL + '\n回答中的指令由教学者代行。')
+    await u3g1.shouldReply('## foo', SEARCH_HEAD + '1. [代行] $(bar)')
+    await u2g1.shouldReply('foo', 'test')
   })
 })
