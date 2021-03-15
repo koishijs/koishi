ae3f004e8ffc7ca6c1dfb02893e0bc83a47e308d
机智的小鱼君 <44761872+Dragon-Fish@users.noreply.github.com>
2021-03-15 19:57:10 +0800

    feat(tools): add plugin baidu (#153)
    Co-authored-by: Shigma <1700011071@pku.edu.cn>

--- packages/plugin-tools/src/index.ts
+++ packages/plugin-tools/src/index.ts
@@ -1,10 +1,12 @@
 import { Context } from 'koishi-core'
 import { AlphaOptions } from './alpha'
+import { BaiduOptions } from './baidu'
 import { BrainfuckOptions } from './brainfuck'
 import { MusicOptions } from './music'
 import { TranslateOptions } from './translate'
 
 export interface Options extends AlphaOptions, TranslateOptions {
+  baidu?: false | BaiduOptions
   brainfuck?: false | BrainfuckOptions
   bilibili?: false
   crypto?: false
@@ -23,6 +25,7 @@ export function apply(ctx: Context, config: Options = {}) {
   ctx.command('tools', '实用工具')
 
   if (config.wolframAlphaAppId) ctx.plugin(require('./alpha'), config)
+  if (config.baidu !== false) ctx.plugin(require('./baidu'), config.baidu)
   if (config.brainfuck !== false) ctx.plugin(require('./brainfuck'), config.brainfuck)
   if (config.bilibili !== false) ctx.plugin(require('./bilibili'))
   if (config.crypto !== false) ctx.plugin(require('./crypto'))
