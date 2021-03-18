7da07841c18dd3c83e42f60e0e0750f7dd66bb78
LittleC <i@ltlec.cn>
2021-03-18 19:46:56 +0800

    fix(discord): embed typing

--- packages/adapter-discord/src/index.ts
+++ packages/adapter-discord/src/index.ts
@@ -2,6 +2,7 @@ import { Adapter } from 'koishi-core'
 import { AxiosRequestConfig } from 'axios'
 import { DiscordBot } from './bot'
 import WsClient from './ws'
+import { Embed } from './types'
 
 export * from './bot'
 export * as dc from './types'
@@ -13,6 +14,12 @@ interface DiscordOptions extends Adapter.WsClientOptions {
 declare module 'koishi-core' {
   interface AppOptions {
     discord?: DiscordOptions
+  }
+
+  interface Session {
+    discord?: {
+      embeds: Embed[]
+    }
   }
 
   namespace Bot {
