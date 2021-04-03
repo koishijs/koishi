9cf079463f5f255fdf6c72adb96be98af3e928b1
Shigma <1700011071@pku.edu.cn>
2021-04-03 15:26:48 +0800

    handle file url

--- packages/adapter-discord/src/bot.ts
+++ packages/adapter-discord/src/bot.ts
@@ -98,23 +98,22 @@ export class DiscordBot extends Bot<'discord'> {
           sentMessageId = r.id
         }
         if (type === 'image' || type === 'video' && data.url) {
-          if (data.url.startsWith('http://') || data.url.startsWith('https://')) {
-            const a = await axios({
-              url: data.url,
-              responseType: 'arraybuffer',
-            })
-            const r = await this.sendEmbedMessage(requestUrl, a.data, {
+          if (data.url.startsWith('file://')) {
+            const r = await this.sendEmbedMessage(requestUrl, readFileSync(data.url.slice(7)), {
               ...addition,
             })
             sentMessageId = r.id
           } else if (data.url.startsWith('base64://')) {
-            const a = Buffer.from(data.url.substring('base64://'.length), 'base64')
+            const a = Buffer.from(data.url.slice(9), 'base64')
             const r = await this.sendEmbedMessage(requestUrl, a, {
               ...addition,
             })
             sentMessageId = r.id
-          } else if (existsSync(data.url)) {
-            const r = await this.sendEmbedMessage(requestUrl, readFileSync(data.url), {
+          } else {
+            const a = await axios.get(data.url, {
+              responseType: 'arraybuffer',
+            })
+            const r = await this.sendEmbedMessage(requestUrl, a.data, {
               ...addition,
             })
             sentMessageId = r.id
