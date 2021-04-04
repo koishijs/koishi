d6dd1289629ae8d066a302105fc61998f0ef8740
Shigma <1700011071@pku.edu.cn>
2021-04-04 22:11:16 +0800

    feat(adv): reorganize import

--- packages/plugin-adventure/src/affinity.ts
+++ packages/plugin-adventure/src/affinity.ts
@@ -135,64 +135,66 @@ export namespace Affinity {
         return output.join('\n')
       })
 
-    ctx.with(['koishi-plugin-teach'], (ctx) => {
-      ctx.command('teach', { patch: true })
-        .option('minAffinity', '-a <aff:posint>  最小好感度')
-        .option('maxAffinity', '-A <aff:posint>  最大好感度')
-        .action(({ options }) => {
-          if (options.maxAffinity === 0) options.maxAffinity = 32768
-        })
+    ctx.with(['koishi-plugin-teach'], integrateTeach)
+  }
 
-      ctx.before('dialogue/search', ({ options }, test) => {
-        if (options.minAffinity !== undefined) test.matchAffinity = options.minAffinity
-        if (options.maxAffinity !== undefined) test.mismatchAffinity = options.maxAffinity
+  function integrateTeach(ctx: Context) {
+    ctx.command('teach', { patch: true })
+      .option('minAffinity', '-a <aff:posint>  最小好感度')
+      .option('maxAffinity', '-A <aff:posint>  最大好感度')
+      .action(({ options }) => {
+        if (options.maxAffinity === 0) options.maxAffinity = 32768
       })
 
-      function matchAffinity(affinity: number) {
-        return `(\`maxAffinity\` > ${affinity} && \`minAffinity\` <= ${affinity})`
+    ctx.before('dialogue/search', ({ options }, test) => {
+      if (options.minAffinity !== undefined) test.matchAffinity = options.minAffinity
+      if (options.maxAffinity !== undefined) test.mismatchAffinity = options.maxAffinity
+    })
+
+    function matchAffinity(affinity: number) {
+      return `(\`maxAffinity\` > ${affinity} && \`minAffinity\` <= ${affinity})`
+    }
+
+    ctx.on('dialogue/mysql', (test, conditionals) => {
+      if (test.matchAffinity !== undefined) {
+        conditionals.push(matchAffinity(test.matchAffinity))
       }
+      if (test.mismatchAffinity !== undefined) {
+        conditionals.push('!' + matchAffinity(test.mismatchAffinity))
+      }
+    })
 
-      ctx.on('dialogue/mysql', (test, conditionals) => {
-        if (test.matchAffinity !== undefined) {
-          conditionals.push(matchAffinity(test.matchAffinity))
-        }
-        if (test.mismatchAffinity !== undefined) {
-          conditionals.push('!' + matchAffinity(test.mismatchAffinity))
-        }
-      })
+    ctx.on('dialogue/modify', async ({ options }, data) => {
+      if (options.minAffinity !== undefined) data.minAffinity = options.minAffinity
+      if (options.maxAffinity !== undefined) data.maxAffinity = options.maxAffinity
+    })
 
-      ctx.on('dialogue/modify', async ({ options }, data) => {
-        if (options.minAffinity !== undefined) data.minAffinity = options.minAffinity
-        if (options.maxAffinity !== undefined) data.maxAffinity = options.maxAffinity
-      })
+    ctx.on('dialogue/detail', (dialogue, output) => {
+      if (dialogue.minAffinity > 0) output.push(`最低好感度：${dialogue.minAffinity}`)
+      if (dialogue.maxAffinity < 32768) output.push(`最高好感度：${dialogue.maxAffinity}`)
+    })
 
-      ctx.on('dialogue/detail', (dialogue, output) => {
-        if (dialogue.minAffinity > 0) output.push(`最低好感度：${dialogue.minAffinity}`)
-        if (dialogue.maxAffinity < 32768) output.push(`最高好感度：${dialogue.maxAffinity}`)
-      })
+    ctx.on('dialogue/detail-short', (dialogue, output) => {
+      if (dialogue.minAffinity > 0) output.push(`a=${dialogue.minAffinity}`)
+      if (dialogue.maxAffinity < 32768) output.push(`A=${dialogue.maxAffinity}`)
+    })
 
-      ctx.on('dialogue/detail-short', (dialogue, output) => {
-        if (dialogue.minAffinity > 0) output.push(`a=${dialogue.minAffinity}`)
-        if (dialogue.maxAffinity < 32768) output.push(`A=${dialogue.maxAffinity}`)
-      })
+    ctx.before('dialogue/attach-user', (state, fields) => {
+      if (state.dialogue) return
+      // 如果所有可能触发的问答都不涉及好感度，则无需获取好感度字段
+      // eslint-disable-next-line no-cond-assign
+      if (state.noAffinityTest = state.dialogues.every(d => !d._weight || !d.minAffinity && d.maxAffinity === 32768)) return
+      for (const field of Affinity.fields) {
+        fields.add(field)
+      }
+    })
 
-      ctx.before('dialogue/attach-user', (state, fields) => {
-        if (state.dialogue) return
-        // 如果所有可能触发的问答都不涉及好感度，则无需获取好感度字段
-        // eslint-disable-next-line no-cond-assign
-        if (state.noAffinityTest = state.dialogues.every(d => !d._weight || !d.minAffinity && d.maxAffinity === 32768)) return
-        for (const field of Affinity.fields) {
-          fields.add(field)
-        }
-      })
-
-      ctx.on('dialogue/attach-user', ({ session, dialogues, noAffinityTest }) => {
-        if (noAffinityTest) return
-        const affinity = Affinity.get(session.user)
-        dialogues.forEach((dialogue) => {
-          if (dialogue.minAffinity <= affinity && dialogue.maxAffinity > affinity) return
-          dialogue._weight = 0
-        })
+    ctx.on('dialogue/attach-user', ({ session, dialogues, noAffinityTest }) => {
+      if (noAffinityTest) return
+      const affinity = Affinity.get(session.user)
+      dialogues.forEach((dialogue) => {
+        if (dialogue.minAffinity <= affinity && dialogue.maxAffinity > affinity) return
+        dialogue._weight = 0
       })
     })
   }
