koishijs/koishi

commit 3b2adf9b44d3f73ddd66db196610d85052a290a1

feat(core): support enum for argv type, fix #337

packages/koishi-core/src/command.ts
@@ -173,10 +173,15 @@ export class Command<U extends User.Field = never, G extends Channel.Field = nev
     return this
   }
 
-  option<K extends string, D extends string, T extends Argv.Type>(name: K, desc: D, config: Argv.OptionConfig<T> = {}) {
+  option<K extends string, T extends keyof Argv.Domain>(name: K, desc: string, config: Argv.OptionConfig<T>): Command<U, G, A, Extend<O, K, Argv.Domain[T]>>
+  option<K extends string, R extends string>(name: K, desc: string, config: Argv.OptionConfig<R[]>): Command<U, G, A, Extend<O, K, R>>
+  option<K extends string, R>(name: K, desc: string, config: Argv.OptionConfig<(source: string) => R>): Command<U, G, A, Extend<O, K, R>>
+  option<K extends string>(name: K, desc: string, config: Argv.OptionConfig<RegExp>): Command<U, G, A, Extend<O, K, string>>
+  option<K extends string, D extends string>(name: K, desc: D, config?: Argv.OptionConfig): Command<U, G, A, Extend<O, K, Argv.OptionType<D>>>
+  option(name: string, desc: string, config: Argv.OptionConfig = {}) {
     this._createOption(name, desc, config)
     this._disposables?.push(() => this.removeOption(name))
-    return this as Command<U, G, A, Extend<O, K, Argv.OptionType<D, T>>>
+    return this
   }
 
   match(session: Session) {

packages/koishi-core/src/parser.ts
@@ -187,15 +187,9 @@ export namespace Argv {
 
   export type ArgumentType<S extends string> = ExtractSpread<Replace<S, '>', ']'>>
 
-  // I don't know why I should write like this but
-  // [T] extends [xxx] just works, so don't touch it
-  export type OptionType<S extends string, T extends Type>
-    = [T] extends [DomainType] ? Domain[T]
-    : [T] extends [RegExp] ? string
-    : T extends (source: string) => infer R ? R
-    : ExtractFirst<Replace<S, '>', ']'>, any>
+  export type OptionType<S extends string> = ExtractFirst<Replace<S, '>', ']'>, any>
 
-  export type Type = DomainType | RegExp | Transform<any>
+  export type Type = DomainType | RegExp | string[] | Transform<any>
 
   export interface Declaration {
     name?: string
@@ -217,10 +211,16 @@ export namespace Argv {
   }
 
   function resolveType(type: Type) {
-    if (typeof type === 'function') return type
-    if (type instanceof RegExp) {
+    if (typeof type === 'function') {
+      return type
+    } else if (type instanceof RegExp) {
       return (source: string) => {
         if (type.test(source)) return source
+        throw new Error()
+      }
+    } else if (Array.isArray(type)) {
+      return (source: string) => {
+        if (type.includes(source)) return source
         throw new Error()
       }
     }

packages/koishi-core/tests/runtime.spec.ts
@@ -265,10 +265,12 @@ describe('Runtime', () => {
       cmd3.option('foo', '<foo>', { type: () => { throw new Error() } })
       cmd3.option('bar', '<bar>', { type: () => { throw new Error('SUFFIX') } })
       cmd3.option('baz', '<baz>', { type: /$^/ })
+      cmd3.option('bax', '<baz>', { type: ['abc', 'def'] })
       await session1.shouldReply('cmd3', 'after cmd3')
       await session1.shouldReply('cmd3 --foo xxx', '选项 foo 输入无效，输入帮助以查看用法。')
       await session1.shouldReply('cmd3 --bar xxx', '选项 bar 输入无效，SUFFIX')
       await session1.shouldReply('cmd3 --baz xxx', '选项 baz 输入无效，输入帮助以查看用法。')
+      await session1.shouldReply('cmd3 --bax cba', '选项 bax 输入无效，输入帮助以查看用法。')
       cmd3.dispose()
     })
 

commit c79fe94085334348d758555b97d210ef2703fe73

fix(core): fix wrong typing for cmd.option()
introduced by 3b2adf9b44d3f73ddd66db196610d85052a290a1

packages/koishi-core/src/command.ts
@@ -173,10 +173,9 @@ export class Command<U extends User.Field = never, G extends Channel.Field = nev
     return this
   }
 
-  option<K extends string, T extends keyof Argv.Domain>(name: K, desc: string, config: Argv.OptionConfig<T>): Command<U, G, A, Extend<O, K, Argv.Domain[T]>>
-  option<K extends string, R extends string>(name: K, desc: string, config: Argv.OptionConfig<R[]>): Command<U, G, A, Extend<O, K, R>>
-  option<K extends string, R>(name: K, desc: string, config: Argv.OptionConfig<(source: string) => R>): Command<U, G, A, Extend<O, K, R>>
-  option<K extends string>(name: K, desc: string, config: Argv.OptionConfig<RegExp>): Command<U, G, A, Extend<O, K, string>>
+  option<K extends string>(name: K, desc: string, config: Argv.TypedOptionConfig<RegExp>): Command<U, G, A, Extend<O, K, string>>
+  option<K extends string, R>(name: K, desc: string, config: Argv.TypedOptionConfig<(source: string) => R>): Command<U, G, A, Extend<O, K, R>>
+  option<K extends string, R extends string>(name: K, desc: string, config: Argv.TypedOptionConfig<R[]>): Command<U, G, A, Extend<O, K, R>>
   option<K extends string, D extends string>(name: K, desc: D, config?: Argv.OptionConfig): Command<U, G, A, Extend<O, K, Argv.OptionType<D>>>
   option(name: string, desc: string, config: Argv.OptionConfig = {}) {
     this._createOption(name, desc, config)

packages/koishi-core/src/help.ts
@@ -125,7 +125,7 @@ function formatCommands(path: string, session: Session<ValidationField>, childre
   return output
 }
 
-function getOptionVisibility(option: Argv.OptionConfig, session: Session<ValidationField>) {
+function getOptionVisibility(option: Argv.OptionDeclaration, session: Session<ValidationField>) {
   if (session.user && option.authority > session.user.authority) return false
   return !session.resolveValue(option.hidden)
 }

packages/koishi-core/src/parser.ts
@@ -355,6 +355,10 @@ export namespace Argv {
     notUsage?: boolean
   }
 
+  export interface TypedOptionConfig<T extends Type> extends OptionConfig<T> {
+    type: T
+  }
+
   export interface OptionDeclaration extends Declaration, OptionConfig {
     description?: string
     values?: Record<string, any>
@@ -377,7 +381,7 @@ export namespace Argv {
       this.declaration = decl.stripped
     }
 
-    _createOption(name: string, def: string, config?: OptionConfig) {
+    _createOption(name: string, def: string, config: OptionConfig) {
       const param = paramCase(name)
       const decl = def.replace(/(?<=^|\s)[\w\x80-\uffff].*/, '')
       const desc = def.slice(decl.length)

packages/plugin-monitor/src/index.ts
@@ -50,7 +50,7 @@ export function apply(ctx: Context) {
   cmd.subcommand('.create <...names>', '添加新的监测账号', { authority: 3 })
     .option('bilibili', '-b <id>  设置 Bilibili 账号')
     .option('mirrativ', '-m <id>  设置 Mirrativ 账号')
-    .option('twitcasting', '-t <id>  设置 TwitCasting 账号', { type: 'string' })
+    .option('twitcasting', '-t <id:string>  设置 TwitCasting 账号')
     .action(async ({ options }, ...names) => {
       if (!names.length) return '请提供至少一个名字。'
 
@@ -96,11 +96,11 @@ export function apply(ctx: Context) {
     })
 
   cmd.subcommand('.update <name>', '修改已有账号信息', { authority: 3 })
-    .option('addName', '-n <name>  添加账号名', { type: 'string', fallback: '' })
-    .option('removeName', '-N <name>  删除账号名', { type: 'string', fallback: '' })
-    .option('bilibili', '-b <id>  设置 Bilibili 账号', { type: 'string' })
-    .option('mirrativ', '-m <id>  设置 Mirrativ 账号', { type: 'string' })
-    .option('twitcasting', '-t <id>  设置 TwitCasting 账号', { type: 'string' })
+    .option('addName', '-n <name:string>  添加账号名', { fallback: '' })
+    .option('removeName', '-N <name:string>  删除账号名', { fallback: '' })
+    .option('bilibili', '-b <id:string>  设置 Bilibili 账号')
+    .option('mirrativ', '-m <id:string>  设置 Mirrativ 账号')
+    .option('twitcasting', '-t <id:string>  设置 TwitCasting 账号')
     .action(async ({ options }, name: string) => {
       if (!name) return '请输入账号。'
       name = String(name)

packages/plugin-puppeteer/src/index.ts
@@ -169,7 +169,7 @@ export function apply(ctx: Context, config: Config = {}) {
   ctx1.command('shot <url> [selector:rawtext]', '网页截图', { authority: 2 })
     .alias('screenshot')
     .option('full', '-f  对整个可滚动区域截图')
-    .option('viewport', '-v <viewport>  指定视口', { type: 'string' })
+    .option('viewport', '-v <viewport:string>  指定视口')
     .action(async ({ session, options }, url, selector) => {
       if (!url) return '请输入网址。'
       const scheme = /^(\w+):\/\//.exec(url)

packages/plugin-tools/src/music.ts
@@ -49,7 +49,7 @@ export function apply(ctx: Context, options: MusicOptions = {}) {
 
   ctx.command('tools/music <name:text>', '点歌')
     // typescript cannot infer type from string templates
-    .option('platform', `-p <platform>  点歌平台，目前支持 qq, netease，默认为 ${platform}`, { type: 'string' })
+    .option('platform', `-p <platform>  点歌平台，目前支持 qq, netease，默认为 ${platform}`, { type: Object.keys(platforms) })
     .alias('点歌')
     .shortcut('来一首', { fuzzy: true })
     .shortcut('点一首', { fuzzy: true })

packages/plugin-tools/src/weather.ts
@@ -10,7 +10,7 @@ export const name = 'weather'
 export function apply(ctx: Context) {
   ctx.command('tools/weather <longitude> <latitude>', '查询天气')
     // typescript cannot infer type from string templates
-    .option('product', `-p <product>  晴天钟产品选择，可为 ${products.join(', ')}`, { type: 'string', fallback: 'civil' })
+    .option('product', `-p <product:string>  晴天钟产品选择，可为 ${products.join(', ')}`, { fallback: 'civil' })
     .action(async ({ options }, lon, lat) => {
       if (!lon || !lat) return '请输入经纬度。'
       const { product } = options

packages/koishi-core/tests/parser.spec.ts
@@ -35,8 +35,8 @@ describe('Parser API', () => {
       cmd.option('beta', '-b <beta>')
       // infer argument type from fallback
       cmd.option('gamma', '-c <gamma>', { fallback: 0 })
-      // define argument type by OptionConfig
-      cmd.option('delta', '-d <delta>', { type: 'string' })
+      // define argument type by definition
+      cmd.option('delta', '-d <delta:string>')
       // define argument type directly (should not be overrode by default)
       cmd.option('epsilon', '-e <epsilon:posint>', { fallback: 1 })
     })