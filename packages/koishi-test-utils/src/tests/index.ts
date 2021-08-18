3dc5b9237a721d05d0a386e6e837922d95e22470
Shigma <1700011071@pku.edu.cn>
2021-08-19 04:48:03 +0800

    finalize memory tests

--- packages/koishi-test-utils/src/tests/index.ts
+++ packages/koishi-test-utils/src/tests/index.ts
@@ -2,27 +2,38 @@ import { ORMTests } from './orm'
 import { App } from 'koishi-core'
 
 export namespace Tests {
-  type UnitOptions<T> = T extends Function ? T extends (...args: infer R) => any ? R[1] : never : {
+  type UnitOptions<T> = (T extends (app: App, options?: infer R) => any ? R : {}) & {
     [K in keyof T]?: false | UnitOptions<T[K]>
   }
 
-  type Unit<T> = T extends Function ? T : ((app: App, options?: UnitOptions<T>) => void) & {
+  type Unit<T> = ((app: App, options?: UnitOptions<T>) => void) & {
     [K in keyof T]: Unit<T[K]>
   }
 
-  function createUnit<T>(target: T): Unit<T> {
-    if (typeof target === 'function') return target as any
-
+  function createUnit<T>(target: T, title?: string): Unit<T> {
     const test: any = (app: App, options: any = {}) => {
-      for (const key in target) {
-        if (options[key] === false) continue
-        test[key](app, options[key])
+      function callback() {
+        if (typeof target === 'function') {
+          target(app, options)
+        }
+
+        for (const key in target) {
+          if (options[key] === false) continue
+          test[key](app, options[key])
+        }
+      }
+
+      if (title) {
+        describe(title, callback)
+      } else {
+        callback()
       }
     }
 
     for (const key in target) {
-      test[key] = createUnit(target[key])
+      test[key] = createUnit(target[key], key.replace(/(?=[A-Z])/g, ' ').trimStart())
     }
+
     return test
   }
 
--- packages/koishi-test-utils/src/tests/orm.ts
+++ packages/koishi-test-utils/src/tests/orm.ts
@@ -19,7 +19,7 @@ declare module 'koishi-core' {
 Tables.extend('foo')
 
 export namespace ORMTests {
-  export function builtin(app: App) {
+  export function BuiltinMethods(app: App) {
     const { database: db } = app
 
     before(() => app.start())
@@ -69,8 +69,18 @@ export namespace ORMTests {
     })
   }
 
-  export namespace query {
-    export function expr(app: App) {
+  export function QueryOperators(app: App) {
+    const db = app.database
+
+    before(async () => {
+      await db.create('foo', { bar: 'awesome foo', baz: 3, list: [], date: new Date('2000-01-01') })
+      await db.create('foo', { bar: 'awesome bar', baz: 4, list: [1] })
+      await db.create('foo', { bar: 'awesome foo bar', baz: 7, list: [100] })
+    })
+  }
+
+  export namespace QueryOperators {
+    export function Comparison(app: App) {
       const db = app.database
 
       it('compile expr query', async () => {
@@ -89,6 +99,162 @@ export namespace ORMTests {
         await expect(db.get('foo', {
           id: { $lt: 1 },
         })).eventually.to.have.length(0)
+      })
+
+      it('should convert date to primitives when doing comparisons', async () => {
+        await expect(db.get('foo', {
+          date: { $eq: new Date('2000-01-01') },
+        })).eventually.to.have.nested.property('[0].bar').equal('awesome foo')
+
+        await expect(db.get('foo', {
+          date: { $gte: new Date('2000-01-01') },
+        })).eventually.to.have.nested.property('[0].bar').equal('awesome foo')
+      })
+    }
+
+    export function Membership(app: App) {
+      const db = app.database
+
+      it('should verify empty array', async () => {
+        await expect(db.get('foo', {
+          id: { $in: [] },
+        })).eventually.to.have.length(0)
+
+        await expect(db.get('foo', {
+          id: { $nin: [] },
+        })).eventually.to.have.length(3)
+      })
+
+      it('filter data by include', async () => {
+        await expect(db.get('foo', {
+          id: { $in: [1, 2] },
+        })).eventually.to.have.length(2)
+
+        await expect(db.get('foo', {
+          id: { $nin: [1] },
+        })).eventually.to.have.length(2)
+      })
+    }
+
+    export function RegularExpression(app: App) {
+      const db = app.database
+
+      it('filter data by regex', async () => {
+        await expect(db.get('foo', {
+          bar: /^.*foo$/,
+        })).eventually.to.have.nested.property('[0].bar').equal('awesome foo')
+
+        await expect(db.get('foo', {
+          bar: {
+            $regex: /^.*foo$/,
+          },
+        })).eventually.to.have.nested.property('[0].bar').equal('awesome foo')
+
+        await expect(db.get('foo', {
+          bar: /^.*foo.*$/,
+        })).eventually.to.have.length(2)
+      })
+    }
+
+    export function Bitwise(app: App) {
+      const db = app.database
+
+      it('filter data by bits', async () => {
+        await expect(db.get('foo', {
+          baz: { $bitsAllSet: 3 },
+        })).eventually.to.have.shape([{ baz: 3 }, { baz: 7 }])
+
+        await expect(db.get('foo', {
+          baz: { $bitsAllClear: 9 },
+        })).eventually.to.have.shape([{ baz: 4 }])
+
+        await expect(db.get('foo', {
+          baz: { $bitsAnySet: 4 },
+        })).eventually.to.have.shape([{ baz: 4 }, { baz: 7 }])
+
+        await expect(db.get('foo', {
+          baz: { $bitsAnyClear: 6 },
+        })).eventually.to.have.shape([{ baz: 3 }, { baz: 4 }])
+      })
+    }
+
+    export function List(app: App) {
+      const db = app.database
+
+      it('filter data by list operators', async () => {
+        await expect(db.get('foo', {
+          list: { $size: 1 },
+        })).eventually.to.have.shape([{ baz: 4 }, { baz: 7 }])
+
+        await expect(db.get('foo', {
+          list: { $el: 100 },
+        })).eventually.to.have.shape([{ baz: 7 }])
+
+        await expect(db.get('foo', {
+          list: { $el: { $lt: 50 } },
+        })).eventually.to.have.shape([{ baz: 4 }])
+      })
+    }
+
+    export function Logical(app: App) {
+      const db = app.database
+
+      it('should verify `$or`, `$and` and `$not`', async () => {
+        await expect(db.get('foo', {
+          $or: [{
+            id: [1, 2],
+          }, {
+            id: [1, 3],
+          }],
+        })).eventually.to.have.length(3)
+
+        await expect(db.get('foo', {
+          $or: [{
+            id: [2],
+          }, {
+            bar: /.*foo.*/,
+          }],
+        })).eventually.to.have.length(3)
+
+        await expect(db.get('foo', {
+          $or: [{
+            id: { $gt: 1 },
+          }, {
+            bar: /.*foo$/,
+          }],
+        })).eventually.to.have.length(3)
+
+        await expect(db.get('foo', {
+          $or: [{ bar: /.*foo/ }, { bar: /foo.*/ }],
+        })).eventually.to.have.length(2)
+
+        await expect(db.get('foo', {
+          $and: [{ bar: /.*foo$/ }, { bar: /foo.*/ }],
+        })).eventually.to.have.length(1)
+
+        await expect(db.get('foo', {
+          $not: { $and: [{ bar: /.*foo$/ }, { bar: /foo.*/ }] },
+        })).eventually.to.have.length(2)
+
+        await expect(db.get('foo', {
+          $not: { $or: [{ bar: /.*foo/ }, { bar: /foo.*/ }] },
+        })).eventually.to.have.length(1)
+      })
+
+      it('should verify `$or` and other key', async () => {
+        await expect(db.get('foo', {
+          bar: /.*foo.*/,
+          $or: [{
+            bar: /.*foo/,
+          }],
+        })).eventually.to.have.length(2)
+
+        await expect(db.get('foo', {
+          bar: /.*foo.*/,
+          $or: [{
+            bar: /foo.+/,
+          }],
+        })).eventually.to.have.length(1)
       })
     }
   }
