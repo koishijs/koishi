59bb9083daf1d12988236336b7f19abf156f804f
thezzisu <thezzisu@gmail.com>
2021-10-07 21:36:24 +0800

    fix(sqlite): change location of test db

--- plugins/sqlite/tests/index.spec.ts
+++ plugins/sqlite/tests/index.spec.ts
@@ -1,15 +1,12 @@
 import { App, Tests } from '@koishijs/test-utils'
 import * as sqlite from '@koishijs/plugin-sqlite'
 import { join } from 'path'
-import { ensureDirSync } from 'fs-extra'
 
 describe(`SQLite Database`, () => {
   const app = new App()
 
-  ensureDirSync(join(__dirname, '..', 'test'))
-
   app.plugin(sqlite, {
-    path: join(__dirname, '..', 'test', 'test.db'),
+    path: join(__dirname, '..', 'tests', 'test.db'),
   })
 
   Tests.database(app, {
