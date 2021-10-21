bb0687bc3c752c3b225a949ee79ef849f7e573c7
Shigma <1700011071@pku.edu.cn>
2021-10-22 01:04:30 +0800

    gocqhttp: fix lifecycle issue

--- plugins/gocqhttp/src/index.ts
+++ plugins/gocqhttp/src/index.ts
@@ -6,15 +6,13 @@ import { resolve } from 'path'
 import { mkdir, copyFile, readFile, writeFile } from 'fs/promises'
 import { URL } from 'url'
 
-declare module 'koishi' {
-  interface Bot {
-    process: ChildProcess
-  }
-}
-
 declare module '@koishijs/plugin-adapter-onebot/lib/bot' {
   interface BotConfig {
     password?: string
+  }
+
+  interface OneBotBot {
+    process: ChildProcess
   }
 }
 
@@ -28,26 +26,32 @@ dict['http'].dict['password'] = Schema.string('机器人的密码。')
 dict['ws'].dict['password'] = Schema.string('机器人的密码。')
 dict['ws-reverse'].dict['password'] = Schema.string('机器人的密码。')
 
-export function apply(ctx: Context) {
-  ctx.on('bot-added', async (bot: onebot.Bot) => {
-    if (bot.adapter.platform !== 'onebot') return
-    const { port, host = 'localhost' } = ctx.app.options
-    const { path = '/onebot/' } = ctx.app.registry.get(onebot).config
-    const cwd = resolve('accounts/' + bot.selfId)
-    await mkdir(cwd, { recursive: true })
-    await copyFile(resolve(__dirname, '../bin/go-cqhttp'), cwd + '/go-cqhttp')
-    const template = await readFile(resolve(__dirname, '../template.yml'), 'utf8')
-    await writeFile(cwd + '/config.yml', interpolate(template, {
-      bot: bot.config,
-      adapter: bot.adapter.config,
-      endpoint: bot.config.endpoint && new URL(bot.config.endpoint),
-      selfUrl: `${host}:${port}${path}`,
-    }, /\$\$(.+)/g))
-    bot.process = spawn('./go-cqhttp', ['faststart'], { stdio: 'inherit', cwd })
-  })
+async function prepare(bot: onebot.Bot) {
+  if (bot.adapter.platform !== 'onebot') return
+  const { port, host = 'localhost' } = bot.app.options
+  const { path = '/onebot/' } = bot.app.registry.get(onebot).config
+  const cwd = resolve('accounts/' + bot.selfId)
+  await mkdir(cwd, { recursive: true })
+  await copyFile(resolve(__dirname, '../bin/go-cqhttp'), cwd + '/go-cqhttp')
+  const template = await readFile(resolve(__dirname, '../template.yml'), 'utf8')
+  await writeFile(cwd + '/config.yml', interpolate(template, {
+    bot: bot.config,
+    adapter: bot.adapter.config,
+    endpoint: bot.config.endpoint && new URL(bot.config.endpoint),
+    selfUrl: `${host}:${port}${path}`,
+  }, /\$\$(.+)/g))
+  bot.process = spawn('./go-cqhttp', ['faststart'], { stdio: 'inherit', cwd })
+}
 
-  ctx.on('bot-removed', async (bot) => {
-    if (bot.adapter.platform !== 'onebot') return
-    bot.process?.kill()
+export function apply(ctx: Context) {
+  ctx.on('connect', () => {
+    ctx.bots.forEach(prepare)
+
+    ctx.on('bot-added', prepare)
+
+    ctx.on('bot-removed', async (bot: onebot.Bot) => {
+      if (bot.adapter.platform !== 'onebot') return
+      bot.process?.kill()
+    })
   })
 }
