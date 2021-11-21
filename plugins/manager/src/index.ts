73c53aba56e79878a933050ebdd6a69a5b93dcfd
Shigma <1700011071@pku.edu.cn>
2021-11-22 03:39:47 +0800

    manager: fix listener this binding error

--- plugins/manager/src/index.ts
+++ plugins/manager/src/index.ts
@@ -40,30 +40,30 @@ export function apply(ctx: Context, config: Config = {}) {
     const filename = ctx.console.config.devMode ? '../client/index.ts' : '../dist/index.js'
     ctx.console.addEntry(resolve(__dirname, filename))
 
-    ctx.console.addListener('install', async ({ name }) => {
+    ctx.console.addListener('install', async function ({ name }) {
       if (await this.validate()) return this.send('unauthorized')
       ctx.console.sources.market.install(name)
     })
 
-    ctx.console.addListener('switch', async ({ name }) => {
+    ctx.console.addListener('switch', async function ({ name }) {
       if (await this.validate()) return this.send('unauthorized')
       ctx.console.sources.registry.switch(name)
     })
 
     for (const event of ['install', 'dispose', 'reload', 'save'] as const) {
-      ctx.console.addListener(`plugin/${event}`, async ({ name, config }) => {
+      ctx.console.addListener(`plugin/${event}`, async function ({ name, config }) {
         if (await this.validate()) return this.send('unauthorized')
         this.app.emit(`config/plugin-${event}`, name, config)
       })
     }
 
-    ctx.console.addListener(`bot/create`, async ({ platform, protocol, config }) => {
+    ctx.console.addListener('bot/create', async function ({ platform, protocol, config }) {
       if (await this.validate()) return this.send('unauthorized')
       this.app.emit('config/bot-create', platform, { protocol, ...config })
     })
 
     for (const event of ['remove', 'start', 'stop'] as const) {
-      ctx.console.addListener(`bot/${event}`, async ({ id }) => {
+      ctx.console.addListener(`bot/${event}`, async function ({ id }) {
         if (await this.validate()) return this.send('unauthorized')
         this.app.emit(`config/bot-${event}`, id)
       })
