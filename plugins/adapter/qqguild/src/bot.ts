dca28683b88c5e4389f6120893cafaafe8ec97da
YiJie <51358815+NWYLZW@users.noreply.github.com>
2021-12-13 03:42:38 +0800

    feat(qqguild): add adapter-qqguild

---
+++ plugins/adapter/qqguild/src/bot.ts
@@ -0,0 +1,29 @@
+import { Bot as GBot, Guild as GGuild } from '@qq-guild-sdk/core'
+import { Bot } from 'koishi'
+import { WebSocketClient } from './ws'
+
+export interface BotConfig extends Bot.BaseConfig, GBot.AppConfig {
+  indents: GBot.Intents | number
+}
+
+const adaptGuild = (guild: GGuild): Bot.Guild => ({
+  guildId: guild.id, guildName: guild.name,
+})
+
+export class QQGuildBot extends Bot<BotConfig> {
+  $innerBot: GBot
+
+  constructor(adapter: WebSocketClient, app: BotConfig) {
+    super(adapter, app)
+    this.$innerBot = new GBot({ app, ...adapter.config })
+  }
+
+  async sendMessage(channelId: string, content: string, guildId?: string): Promise<string> {
+    const resp = await this.$innerBot.send.channel(channelId, content)
+    return resp.id
+  }
+
+  async getGuildList() {
+    return this.$innerBot.guilds.then(guilds => guilds.map(adaptGuild))
+  }
+}
---
+++ plugins/adapter/qqguild/src/ws.ts
@@ -0,0 +1,58 @@
+import { Bot as GBot, Message } from '@qq-guild-sdk/core'
+import { Adapter, Logger, Schema, segment, Session } from 'koishi'
+import { BotConfig, QQGuildBot } from './bot'
+
+const logger = new Logger('qqguild')
+
+export interface AdapterConfig extends Adapter.WebSocketClient.Config, Omit<GBot.Options, 'app'> {
+}
+
+export const AdapterConfig: Schema<AdapterConfig> = Schema.intersect([
+  Schema.object({
+    sandbox: Schema.boolean()
+      .description('是否开启沙盒')
+      .default(true),
+    endpoint: Schema.string()
+      .description('api 入口地址')
+      .default('https://api.sgroup.qq.com/'),
+    authType: Schema.union(['bot', 'bearer'])
+      .description('验证方式')
+      .default('bot'),
+  }),
+  Adapter.WebSocketClient.Config,
+])
+
+const createSession = (bot: QQGuildBot, msg: Message) => {
+  const { id: messageId, guildId, channelId, timestamp } = msg
+  const session: Partial<Session> = {
+    selfId: bot.selfId,
+    guildId,
+    messageId,
+    channelId,
+    timestamp: +timestamp,
+  }
+  session.guildId = msg.guildId
+  session.channelId = msg.channelId
+  session.subtype = 'group'
+  session.content = msg.content
+    .replace(/<@!(.+)>/, (_, $1) => segment.at($1))
+    .replace(/<#(.+)>/, (_, $1) => segment.sharp($1))
+  return new Session(bot, session)
+}
+
+export class WebSocketClient extends Adapter<BotConfig, AdapterConfig> {
+  async connect(bot: QQGuildBot) {
+    await bot.$innerBot.startClient(bot.config.indents)
+    bot.$innerBot.on('ready', bot.resolve)
+    bot.$innerBot.on('message', msg => {
+      const session = createSession(bot, msg)
+      if (session) this.dispatch(session)
+    })
+  }
+
+  start() { }
+
+  stop() {
+    logger.debug('ws server closing')
+  }
+}
