4d7db6e5533487f255647975ad61a1c24d311e7a
yijie4188 <yijie4188@gmail.com>
2021-12-13 14:43:04 +0800

    feat(adapter): realize getSelf method

--- plugins/adapter/qqguild/src/bot.ts
+++ plugins/adapter/qqguild/src/bot.ts
@@ -1,6 +1,7 @@
-import { Bot as GBot, Guild as GGuild } from '@qq-guild-sdk/core'
+import { Bot as GBot, Guild as GGuild, User as GUser } from '@qq-guild-sdk/core'
 import { Bot } from 'koishi'
 import { WebSocketClient } from './ws'
+import { renameProperty } from '@koishijs/utils'
 
 export interface BotConfig extends Bot.BaseConfig, GBot.AppConfig {
   indents: GBot.Intents | number
@@ -10,11 +11,25 @@ const adaptGuild = (guild: GGuild): Bot.Guild => ({
   guildId: guild.id, guildName: guild.name,
 })
 
+const adaptUser = (user: GUser): Bot.User => ({
+  isBot: user.bot,
+  avatar: user.avatar,
+  userId: user.id,
+  username: user.username,
+  nickname: user.username,
+})
+
 export class QQGuildBot extends Bot<BotConfig> {
   $innerBot: GBot
   constructor(adapter: WebSocketClient, app: BotConfig) {
     super(adapter, app)
     this.$innerBot = new GBot({ app, ...adapter.config })
+  }
+
+  async getSelf() {
+    const u = adaptUser(await this.$innerBot.me)
+    renameProperty(u, 'selfId' as never, 'userId')
+    return u
   }
 
   async sendMessage(channelId: string, content: string, guildId?: string): Promise<string> {
