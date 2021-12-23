571ca8fd12ee3979a145243aba29ea48bd5bf632
purerosefallen <78877@qq.com>
2021-12-23 21:15:59 +0800

    fix tiny_id detection

--- plugins/adapter/onebot/src/bot.ts
+++ plugins/adapter/onebot/src/bot.ts
@@ -34,7 +34,7 @@ export class OneBotBot extends Bot<BotConfig> {
   async initializeGuildServiceProfile() {
     try {
       const profile = await this.internal.getGuildServiceProfile()
-      if (!profile?.tiny_id) {
+      if (!profile?.tiny_id || profile.tiny_id === 0) {
         // Guild service is not supported in this account
         return
       }
