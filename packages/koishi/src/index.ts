bc0150ea9a790dcb999bfb41c8977bf85055c726
Shigma <1700011071@pku.edu.cn>
2021-12-26 01:31:10 +0800

    core: fix http listening issue

--- packages/koishi/src/index.ts
+++ packages/koishi/src/index.ts
@@ -39,3 +39,18 @@ App.prototype.prepare = function (this: App, ...args) {
   prepare.call(this, ...args)
   Router.prepare(this)
 }
+
+const start = App.prototype.start
+App.prototype.start = function (this: App, ...args) {
+  const { port, host = 'localhost' } = this.options
+  if (port) {
+    this._httpServer.listen(port, host)
+    this.logger('app').info('server listening at %c', `http://${host}:${port}`)
+    this.on('dispose', () => {
+      this.logger('app').info('http server closing')
+      this._wsServer?.close()
+      this._httpServer?.close()
+    })
+  }
+  return start.call(this, ...args)
+}
