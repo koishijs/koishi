3e860d17428a2d57760accb18b1ab6215dc5068c
Shigma <1700011071@pku.edu.cn>
2022-01-01 18:32:56 +0800

    admin: use separate plugin files and migrate to cmd.use()

---
+++ plugins/admin/src/bind.ts
@@ -0,0 +1,77 @@
+import { Context, Dict, Random, Session, template, Time, User } from 'koishi'
+
+template.set('bind', {
+  'generated-1': [
+    'bind 指令可用于在多个平台间绑定用户数据。绑定过程中，源平台的用户数据将完全保留，而目标平台的用户数据将被源平台的数据所覆盖。',
+    '请确认当前平台是你的目标平台，并在 5 分钟内使用你的账号在源平台内向机器人发送以下文本：',
+    '{0}',
+    '注意：每个账号只能绑定到每个平台一次，此操作将会抹去你当前平台上的数据，请谨慎操作！',
+  ].join('\n'),
+  'generated-2': [
+    '令牌核验成功！下面将进行第二步操作。',
+    '请在 5 分钟内使用你的账号在目标平台内向机器人发送以下文本：',
+    '{0}',
+    '注意：当前平台是你的源平台，这里的用户数据将完全保留，而目标平台的用户数据将被覆盖，请谨慎操作！',
+  ].join('\n'),
+  'failed': '账号绑定失败：你已经绑定过该平台。',
+  'success': '账号绑定成功！',
+})
+
+export interface Config {
+  generateToken?: () => string
+}
+
+export const name = 'bind'
+
+export function apply(ctx: Context, config: Config = {}) {
+  // 1: group (1st step)
+  // 0: private
+  // -1: group (2nd step)
+  type TokenData = [platform: string, id: string, pending: number]
+  const tokens: Dict<TokenData> = {}
+
+  const { generateToken = () => 'koishi/' + Random.id(6, 10) } = config
+
+  function generate(session: Session, pending: number) {
+    const token = generateToken()
+    tokens[token] = [session.platform, session.userId, pending]
+    setTimeout(() => delete tokens[token], 5 * Time.minute)
+    return token
+  }
+
+  async function bind(user: User.Observed<never>, platform: string, userId: string) {
+    await ctx.database.remove('user', { [platform]: [userId] })
+    user[platform] = userId as never
+    await user.$update()
+  }
+
+  ctx.command('common/bind', '绑定到账号', { authority: 0 })
+    .action(({ session }) => {
+      const token = generate(session, +(session.subtype === 'group'))
+      return template('bind.generated-1', token)
+    })
+
+  ctx.middleware(async (session, next) => {
+    const data = tokens[session.content]
+    if (!data) return next()
+    if (data[2] < 0) {
+      const sess = new Session(session.bot, { ...session, platform: data[0], userId: data[1] })
+      const user = await sess.observeUser([session.platform as never])
+      delete tokens[session.content]
+      await bind(user, session.platform, session.userId)
+      return template('bind.success')
+    } else {
+      const user = await session.observeUser(['authority', data[0] as never])
+      if (!user.authority) return template('internal.low-authority')
+      if (user[data[0]]) return template('bind.failed')
+      delete tokens[session.content]
+      if (data[2]) {
+        const token = generate(session, -1)
+        return template('bind.generated-2', token)
+      } else {
+        await bind(user, data[0], data[1])
+        return template('bind.success')
+      }
+    }
+  }, true)
+}
---
+++ plugins/admin/src/callme.ts
@@ -0,0 +1,58 @@
+import { Context, KoishiError, template } from 'koishi'
+
+declare module 'koishi' {
+  interface EventMap {
+    'common/callme'(name: string, session: Session): string | void
+  }
+}
+
+template.set('callme', {
+  'current': '好的呢，{0}！',
+  'unnamed': '你还没有给自己起一个称呼呢~',
+  'unchanged': '称呼未发生变化。',
+  'empty': '称呼不能为空。',
+  'invalid': '称呼中禁止包含纯文本以外的内容。',
+  'duplicate': '禁止与其他用户重名。',
+  'updated': '好的，{0}，请多指教！',
+  'failed': '修改称呼失败。',
+})
+
+export const name = 'callme'
+
+export function apply(ctx: Context) {
+  ctx.command('common/callme [name:text]', '修改自己的称呼')
+    .userFields(['id', 'name'])
+    .shortcut('叫我', { prefix: true, fuzzy: true })
+    .action(async ({ session }, name) => {
+      const { user } = session
+      if (!name) {
+        if (user.name) {
+          return template('callme.current', session.username)
+        } else {
+          return template('callme.unnamed')
+        }
+      } else if (name === user.name) {
+        return template('callme.unchanged')
+      } else if (!(name = name.trim())) {
+        return template('callme.empty')
+      } else if (name.includes('[CQ:')) {
+        return template('callme.invalid')
+      }
+
+      const result = ctx.bail('common/callme', name, session)
+      if (result) return result
+
+      try {
+        user.name = name
+        await user.$update()
+        return template('callme.updated', session.username)
+      } catch (error) {
+        if (KoishiError.check(error, 'database.duplicate-entry')) {
+          return template('callme.duplicate')
+        } else {
+          ctx.logger('common').warn(error)
+          return template('callme.failed')
+        }
+      }
+    })
+}
