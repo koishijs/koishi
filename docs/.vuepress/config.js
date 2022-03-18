99021adebf761e0e2c637dcd69153da837e8c2d6
迷子 (Maiko Tan) <maiko.tan.coding@gmail.com>
2022-03-18 09:23:13 +0000

    add md to config

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -128,6 +128,7 @@ module.exports = {
         isGroup: true,
         children: [
           '/guide/i18n/index.md',
+          '/guide/i18n/crowdin.md',
         ],
       }, {
         text: '控制台开发',
