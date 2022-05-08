aa85078e7301d292b1e468c30e88bd547cd8b73b
yijie4188 <yijie4188@gmail.com>
2022-05-08 17:30:46 +0800

    fix: fix attachments is not resolve and support resolve string intents
    #622

--- plugins/adapter/qqguild/src/ws.ts
+++ plugins/adapter/qqguild/src/ws.ts
@@ -1,4 +1,5 @@
 import { Message } from '@qq-guild-sdk/core'
+import * as QQGuild from '@qq-guild-sdk/core'
 import { Logger, segment } from '@koishijs/utils'
 import { Adapter, Session } from 'koishi'
 import { QQGuildBot } from './bot'
@@ -23,18 +24,35 @@ const createSession = (bot: QQGuildBot, msg: Message) => {
   session.channelId = msg.channelId
   session.subtype = 'group'
   session.content = msg.content
-    .replace(/<@!(.+)>/, (_, $1) => segment.at($1))
-    .replace(/<#(.+)>/, (_, $1) => segment.sharp($1))
+    ?.replace(/<@!(.+)>/, (_, $1) => segment.at($1))
+    ?.replace(/<#(.+)>/, (_, $1) => segment.sharp($1))
+    ?? ''
+  session.content = (msg as any as { attachments: any[] }).attachments
+    .filter(({ contentType }) => contentType.startsWith('image'))
+    .reduce(
+      (content, attachment) => content + segment.image(attachment.url), session.content,
+    )
   return new Session(bot, session)
 }
 
 export class WebSocketClient extends Adapter<BotConfig, AdapterConfig> {
   static schema = BotConfig
 
+  resolveIntents(intents: BotConfig['intents']): number {
+    if (typeof intents === 'number') {
+      return intents
+    }
+    Array.isArray(intents) || (intents = [intents])
+    return intents.reduce((sum, intent) => sum | QQGuild.Bot.Intents[intent], 0)
+  }
+
   async connect(bot: QQGuildBot) {
     Object.assign(bot, await bot.getSelf())
     bot.resolve()
-    await bot.$innerBot.startClient(bot.config.indents)
+    if (bot.config.intents === undefined) {
+      throw new Error('intents is required')
+    }
+    await bot.$innerBot.startClient(this.resolveIntents(bot.config.intents))
     bot.$innerBot.on('ready', bot.resolve)
     bot.$innerBot.on('message', msg => {
       const session = createSession(bot, msg)
