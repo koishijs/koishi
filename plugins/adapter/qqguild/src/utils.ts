0f441d08985be38e5a404c24b7e9120e2ee8a7ef
yijie4188 <yijie4188@gmail.com>
2022-05-09 00:04:13 +0800

    feat(adapter-qqguild): supplement intents description

--- plugins/adapter/qqguild/src/utils.ts
+++ plugins/adapter/qqguild/src/utils.ts
@@ -24,7 +24,7 @@ export const BotConfig = Schema.intersect([
     id: Schema.string().description('机器人 id。').required(),
     key: Schema.string().description('机器人 key。').role('secret').required(),
     token: Schema.string().description('机器人令牌。').role('secret').required(),
-    intents: Schema.number(),
+    intents: Schema.number().description('需要订阅的机器人事件。'),
   }),
 ])
 
