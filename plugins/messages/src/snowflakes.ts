fa104692b889b297db1f9f46162aa16f3a91905d
LittleC <i@ltlec.cn>
2022-05-09 00:17:30 +0800

    feat(messages): increment

--- plugins/messages/src/snowflakes.ts
+++ plugins/messages/src/snowflakes.ts
@@ -1,10 +1,14 @@
 const epoch = 1574773581000n // Tue Nov 26 21:06:21 2019 +0800, \koishi/
 const TIMESTAMP_SHIFT = 22n
 
+let increment = 0n
+
 function snowflakes() {
   const ts = Date.now()
-  return ((BigInt(ts) - BigInt(epoch)) << TIMESTAMP_SHIFT)
-    + BigInt(Math.round(Math.random() * (Math.pow(2, 12) - 1)))
+  const result = ((BigInt(ts) - BigInt(epoch)) << TIMESTAMP_SHIFT)
+    + (increment++)
+  increment %= 0xFFFn
+  return result
 }
 
 // https://discord.com/developers/docs/reference#snowflakes
