c41eec69d208739666d442615b271ff7232bdec0
迷子 (Maiko Tan) <maiko.tan.coding@gmail.com>
2022-05-09 07:59:17 +0000

    refactor: a bit internal (from discord)

---
+++ plugins/adapter/feishu/src/types/internal.ts
@@ -0,0 +1,41 @@
+import { Dict, makeArray, Quester } from 'koishi'
+import { AxiosRequestConfig } from 'axios'
+
+export interface Internal {}
+
+type Method = 'GET' | 'POST' | 'PUT' | 'DELETE' | 'PATCH'
+
+export class Internal {
+  constructor(private http: Quester) {}
+
+  static define(routes: Dict<Partial<Record<Method, string | string[]>>>) {
+    for (const path in routes) {
+      for (const key in routes[path]) {
+        const method = key as Method
+        for (const name of makeArray(routes[path][method])) {
+          Internal.prototype[name] = function (this: Internal, ...args: any[]) {
+            const raw = args.join(', ')
+            const url = path.replace(/\{([^}]+)\}/g, () => {
+              if (!args.length) throw new Error(`too few arguments for ${path}, received ${raw}`)
+              return args.shift()
+            })
+            const config: AxiosRequestConfig = {}
+            if (args.length === 1) {
+              if (method === 'GET' || method === 'DELETE') {
+                config.params = args[0]
+              } else {
+                config.data = args[0]
+              }
+            } else if (args.length === 2 && method !== 'GET' && method !== 'DELETE') {
+              config.data = args[0]
+              config.params = args[1]
+            } else if (args.length > 1) {
+              throw new Error(`too many arguments for ${path}, received ${raw}`)
+            }
+            return this.http(method, url, config)
+          }
+        }
+      }
+    }
+  }
+}
