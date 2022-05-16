fa05a40302244da4ffa0dd86130771d7f0102f3e
迷子 (Maiko Tan) <maiko.tan.coding@gmail.com>
2022-05-16 02:55:07 +0000

    feat: add getAppAccessToken function
    NOTE: According to feishu, when you a building an app as
    a "exterprise custom app", then `app_access_token` is equivalent as
    `tenant_access_token`.

--- plugins/adapter/feishu/src/types/auth.ts
+++ plugins/adapter/feishu/src/types/auth.ts
@@ -1,5 +1,12 @@
 import { BaseResponse, Internal } from '.'
 
+export interface AppAccessToken extends BaseResponse {
+  /** access token */
+  app_access_token: string
+  /** expire time in seconds. e.g: 7140 (119 minutes) */
+  expire: number
+}
+
 export interface TenantAccessToken extends BaseResponse {
   /** access token */
   tenant_access_token: string
@@ -9,11 +16,23 @@ export interface TenantAccessToken extends BaseResponse {
 
 declare module './internal' {
   export interface Internal {
+    /**
+     * Returns the app_access_token for the bot.
+     * @see https://open.feishu.cn/document/ukTMukTMukTM/ukDNz4SO0MjL5QzM/auth-v3/auth/app_access_token_internal
+     */
+    getAppAccessToken(app_id: string, app_secret: string): Promise<AppAccessToken>
+    /**
+     * Returns the tenant_access_token for the bot.
+     * @see https://open.feishu.cn/document/ukTMukTMukTM/ukDNz4SO0MjL5QzM/auth-v3/auth/tenant_access_token_internal
+     */
     getTenantAccessToken(app_id: string, app_secret: string): Promise<TenantAccessToken>
   }
 }
 
 Internal.define({
+  'auth/v3/app_access_token/internal': {
+    POST: 'getAppAccessToken',
+  },
   'auth/v3/tenant_access_token/internal': {
     POST: 'getTenantAccessToken',
   },
