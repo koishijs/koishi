7486104db1da0919411cba49ee79fbe41a46dc33
idanran <96647698+idanran@users.noreply.github.com>
2022-05-24 12:52:41 +0800

    fix(database-mongo): Windows does not listen to localhost by default

--- plugins/database/mongo/src/index.ts
+++ plugins/database/mongo/src/index.ts
@@ -7,7 +7,7 @@ export type Config = MongoDriver.Config
 
 export const Config: Schema<Config> = Schema.object({
   protocol: Schema.string().description('要使用的协议名。').default('mongodb'),
-  host: Schema.string().description('要连接到的主机名。').default('localhost'),
+  host: Schema.string().description('要连接到的主机名。').default('127.0.0.1'),
   port: Schema.natural().max(65535).description('要连接到的端口号。'),
   username: Schema.string().description('要使用的用户名。'),
   password: Schema.string().description('要使用的密码。').role('secret'),
