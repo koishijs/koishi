aa10c5f5efe862fa8b1d5a634ffd4a73e65d06af
idanran <96647698+idanran@users.noreply.github.com>
2022-05-24 13:41:30 +0800

    Update index.ts

--- plugins/database/mongo/src/index.ts
+++ plugins/database/mongo/src/index.ts
@@ -8,7 +8,7 @@ export type Config = MongoDriver.Config
 export const Config: Schema<Config> = Schema.object({
   protocol: Schema.string().description('要使用的协议名。').default('mongodb'),
   host: Schema.string().description('要连接到的主机名。').default('127.0.0.1'),
-  port: Schema.natural().max(65535).description('要连接到的端口号。').default(),
+  port: Schema.natural().max(65535).description('要连接到的端口号。'),
   username: Schema.string().description('要使用的用户名。'),
   password: Schema.string().description('要使用的密码。').role('secret'),
   database: Schema.string().description('要访问的数据库名。').default('koishi'),
