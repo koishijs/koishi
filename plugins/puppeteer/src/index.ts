4051a5cd0d17c6a235ff778a390690f5a59f9415
Shigma <shigma10826@gmail.com>
2022-06-30 04:32:25 +0800

    feat(puppeteer): add more config fields

--- plugins/puppeteer/src/index.ts
+++ plugins/puppeteer/src/index.ts
@@ -19,14 +19,21 @@ declare module 'koishi' {
 
 type LaunchOptions = Parameters<typeof puppeteer.launch>[0]
 
-const LaunchOptions = Schema.object({
-  executablePath: Schema.string().description('Chromium 可执行文件的路径。缺省时将自动从系统中寻找。'),
-  defaultViewport: Schema.object({
-    width: Schema.natural().description('默认的视图宽度。').default(800),
-    height: Schema.natural().description('默认的视图高度。').default(600),
-    deviceScaleFactor: Schema.number().min(0).description('默认的设备缩放比率。').default(2),
-  }),
-}).description('浏览器设置')
+const LaunchOptions = Schema.intersect([
+  Schema.object({
+    executablePath: Schema.string().description('Chromium 可执行文件的路径。缺省时将自动从系统中寻找。'),
+    defaultViewport: Schema.object({
+      width: Schema.natural().description('默认的视图宽度。').default(800),
+      height: Schema.natural().description('默认的视图高度。').default(600),
+      deviceScaleFactor: Schema.number().min(0).description('默认的设备缩放比率。').default(2),
+    }),
+  }).description('浏览器设置'),
+  Schema.object({
+    headless: Schema.boolean().description('是否开启[无头模式](https://developer.chrome.com/blog/headless-chrome/)。').default(true),
+    ignoreHTTPSErrors: Schema.boolean().description('在导航时忽略 HTTPS 错误。').default(false),
+    args: Schema.array(String).description('额外的浏览器参数。Chromium 参数可以参考[这个页面](https://peter.sh/experiments/chromium-command-line-switches/)。'),
+  }).description('高级设置'),
+])
 
 type RenderCallback = (page: Page, next: (handle?: ElementHandle) => Promise<string>) => Promise<string>
 
