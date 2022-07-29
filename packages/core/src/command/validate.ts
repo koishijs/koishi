92fd18817514de126b3a5f0646e897fde735a734
Shiroha <17447826+Shir0ha@users.noreply.github.com>
2022-07-30 05:20:24 +1000

    lint: Strings must use singlequote

--- packages/core/src/command/validate.ts
+++ packages/core/src/command/validate.ts
@@ -29,7 +29,7 @@ export default function validate(ctx: Context) {
     }
 
     // check authority
-    if (typeof session.user.authority === "number") {
+    if (typeof session.user.authority === 'number') {
       const authority = command.getConfig('authority', session)
       if (authority > session.user.authority) {
         return sendHint('internal.low-authority')
