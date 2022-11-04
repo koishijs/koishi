ae3c566184529d562acf261cbf53325a96bb3ca6
Shigma <shigma10826@gmail.com>
2022-11-05 02:40:03 +0800

    refa: drop support for @koishijs/plugin-database-level

--- plugins/database/level/src/index.ts
+++
@@ -1,6 +0,0 @@
-import { defineDriver, Schema } from 'koishi'
-import LevelDriver from '@minatojs/driver-level'
-
-export default defineDriver(LevelDriver, Schema.object({
-  location: Schema.string().description('数据保存的位置').default('.level'),
-}))
