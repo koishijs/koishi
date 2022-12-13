koishijs/koishi

commit c69a5b7cdcb69b251e9b174738f6792244a05c32

feat(loader): support interpolations returning non-strings, fix #641
with unhandled frontend schema error, which will be tracked in another issue in koishijs/console

packages/utils/src/string.ts
@@ -1,16 +1,26 @@
 // eslint-disable-next-line no-new-func
-export const interpolate = new Function('template', 'context', 'pattern', `
-  return template.replace(pattern || /\\{\\{([\\s\\S]+?)\\}\\}/g, (_, expr) => {
-    try {
-      with (context) {
-        const result = eval(expr)
-        return result === undefined ? '' : result
-      }
-    } catch {
-      return ''
+const evaluate = new Function('context', 'expr', `
+  try {
+    with (context) {
+      return eval(expr)
     }
-  })
-`) as ((template: string, context: object, pattern?: RegExp) => string)
+  } catch {}
+`) as ((context: object, expr: string) => any)
+
+export function interpolate(template: string, context: object, pattern = /\{\{([\s\S]+?)\}\}/g) {
+  let capture: RegExpExecArray
+  let result = '', hasMatch = false
+  while ((capture = pattern.exec(template))) {
+    if (!hasMatch && capture[0] === template) {
+      return evaluate(context, capture[1])
+    }
+    hasMatch = true
+    result += template.slice(0, capture.index)
+    result += evaluate(context, capture[1]) ?? ''
+    template = template.slice(pattern.lastIndex)
+  }
+  return result + template
+}
 
 export function escapeRegExp(source: string) {
   return source

packages/loader/src/node.ts
@@ -76,6 +76,7 @@ export default class NodeLoader extends Loader {
   }
 
   interpolate(source: any) {
+    if (!this.writable) return source
     if (typeof source === 'string') {
       return interpolate(source, context, /\$\{\{(.+?)\}\}/g)
     } else if (!source || typeof source !== 'object') {
@@ -101,14 +102,7 @@ export default class NodeLoader extends Loader {
       this.config = module.default || module
     }
 
-    let resolved = new Context.Config(this.config)
-    if (this.writable) {
-      // schemastery may change original config
-      // so we need to validate config twice
-      resolved = new Context.Config(this.interpolate(this.config))
-    }
-
-    return resolved
+    return new Context.Config(this.interpolate(this.config))
   }
 
   writeConfig() {

packages/loader/src/shared.ts
@@ -142,7 +142,7 @@ export abstract class Loader {
   }
 
   async createApp() {
-    const app = this.app = new Context(this.config)
+    const app = this.app = new Context(this.interpolate(this.config))
     app.loader = this
     app.baseDir = this.baseDir
     app.state[Loader.kRecord] = Object.create(null)

packages/utils/tests/string.spec.ts
@@ -5,6 +5,7 @@ describe('String Manipulations', () => {
   it('interpolate', () => {
     expect(interpolate('foo{{ bar }}foo', { bar: 'baz' })).to.equal('foobazfoo')
     expect(interpolate('foo{{ bar }}foo', {})).to.equal('foofoo')
+    expect(interpolate('{{ bar }}', {})).to.equal(undefined)
   })
 
   it('escape regexp', () => {

commit f5ef84ba056f6d754e93f412bdc2be2c40251c58

feat(loader): fix regression bug
introduced by c69a5b7cdcb69b251e9b174738f6792244a05c32

packages/utils/src/string.ts
@@ -9,17 +9,16 @@ const evaluate = new Function('context', 'expr', `
 
 export function interpolate(template: string, context: object, pattern = /\{\{([\s\S]+?)\}\}/g) {
   let capture: RegExpExecArray
-  let result = '', hasMatch = false
+  let result = '', lastIndex = 0
   while ((capture = pattern.exec(template))) {
-    if (!hasMatch && capture[0] === template) {
+    if (capture[0] === template) {
       return evaluate(context, capture[1])
     }
-    hasMatch = true
-    result += template.slice(0, capture.index)
+    result += template.slice(lastIndex, capture.index)
     result += evaluate(context, capture[1]) ?? ''
-    template = template.slice(pattern.lastIndex)
+    lastIndex = capture.index + capture[0].length
   }
-  return result + template
+  return result + template.slice(lastIndex)
 }
 
 export function escapeRegExp(source: string) {