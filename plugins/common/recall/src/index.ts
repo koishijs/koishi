dd87d6ee8027f924e6b84b9172b4449dc85679a4
Anillc <i@anillc.cn>
2023-01-26 03:39:11 +0800

    feat(recall): recall reply

--- plugins/common/recall/src/index.ts
+++ plugins/common/recall/src/index.ts
@@ -1,4 +1,4 @@
-import { Context, Dict, remove, Schema, sleep, Time } from 'koishi'
+import { Bot, Context, Dict, remove, Schema, sleep, Time } from 'koishi'
 import zh from './locales/zh.yml'
 
 export interface Config {
@@ -12,6 +12,7 @@ export const Config: Schema<Config> = Schema.object({
 export const name = 'recall'
 
 export function apply(ctx: Context, { timeout }: Config) {
+  const logger = ctx.logger('recall')
   ctx.i18n.define('zh', zh)
 
   ctx = ctx.guild()
@@ -26,17 +27,27 @@ export function apply(ctx: Context, { timeout }: Config) {
   ctx.command('recall [count:number]', { authority: 2 })
     .action(async ({ session }, count = 1) => {
       const list = recent[session.channelId]
+      if (session.quote) {
+        const index = list?.findIndex(id => id === session.quote.messageId)
+        if (index) list.splice(index, 1)
+        await deleteMessage(session.bot, session.channelId, session.quote.messageId)
+        return
+      }
       if (!list) return session.text('.no-recent')
       const removal = list.splice(0, count)
       const delay = ctx.root.config.delay.broadcast
       if (!list.length) delete recent[session.channelId]
       for (let index = 0; index < removal.length; index++) {
         if (index && delay) await sleep(delay)
-        try {
-          await session.bot.deleteMessage(session.channelId, removal[index])
-        } catch (error) {
-          ctx.logger('bot').warn(error)
-        }
+        await deleteMessage(session.bot, session.channelId, removal[index])
       }
     })
+
+  async function deleteMessage(bot: Bot, channelId: string, messageId: string) {
+    try {
+      await bot.deleteMessage(channelId, messageId)
+    } catch (error) {
+      logger.warn(error)
+    }
+  }
 }
