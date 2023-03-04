3ec508654dfddd33bc7486e0d7647e7ff606afd2
Shigma <shigma10826@gmail.com>
2023-03-05 03:02:12 +0800

    refa: drop exit command in favor of koishi-plugin-shutdown

--- packages/koishi/src/worker/daemon.ts
+++ packages/koishi/src/worker/daemon.ts
@@ -1,31 +1,12 @@
-import { Context, noop, Schema } from '@koishijs/core'
-import zhCN from '../locales/zh-CN.yml'
-import jaJP from '../locales/ja-JP.yml'
-import frFR from '../locales/fr-FR.yml'
-import zhTW from '../locales/zh-TW.yml'
-
-declare module '@koishijs/core' {
-  interface EnvData {
-    startMessage?: StartMessage
-  }
-}
-
-interface StartMessage {
-  channelId: string
-  guildId: string
-  sid: string
-  message: string
-}
+import { Context, Schema } from '@koishijs/core'
 
 export interface Config {
-  exitCommand?: boolean
   autoRestart?: boolean
   heartbeatInterval?: number
   heartbeatTimeout?: number
 }
 
 export const Config: Schema<Config> = Schema.object({
-  exitCommand: Schema.boolean().description('提供退出指令。').default(false),
   autoRestart: Schema.boolean().description('在运行时崩溃自动重启。').default(true),
   heartbeatInterval: Schema.number().description('心跳发送间隔。').default(0),
   heartbeatTimeout: Schema.number().description('心跳超时时间。').default(0),
@@ -47,26 +28,6 @@ export function apply(ctx: Context, config: Config = {}) {
     ctx.parallel('exit', signal).finally(() => process.exit())
   }
 
-  ctx.i18n.define('zh', zhCN)
-  ctx.i18n.define('ja', jaJP)
-  ctx.i18n.define('fr', frFR)
-  ctx.i18n.define('zh-TW', zhTW)
-
-  ctx
-    .intersect(() => config.exitCommand)
-    .command('exit', { authority: 4 })
-    .option('restart', '-r')
-    .action(async ({ options, session }) => {
-      const { channelId, guildId, sid } = session
-      if (!options.restart) {
-        await session.send(session.text('.exiting')).catch(noop)
-        process.exit()
-      }
-      await session.send(session.text('.restarting')).catch(noop)
-      ctx.envData.startMessage = { channelId, guildId, sid, message: session.text('.restarted') }
-      ctx.loader.fullReload()
-    })
-
   ctx.on('ready', () => {
     process.send({ type: 'start', body: config })
     process.on('SIGINT', handleSignal)
@@ -75,15 +36,5 @@ export function apply(ctx: Context, config: Config = {}) {
     config.heartbeatInterval && setInterval(() => {
       process.send({ type: 'heartbeat' })
     }, config.heartbeatInterval)
-
-    if (ctx.envData.startMessage) {
-      const { channelId, guildId, sid, message } = ctx.envData.startMessage
-      const dispose = ctx.on('bot-status-updated', (bot) => {
-        if (bot.sid !== sid || bot.status !== 'online') return
-        bot.sendMessage(channelId, message, guildId)
-        dispose()
-      })
-      ctx.envData.startMessage = null
-    }
   })
 }
