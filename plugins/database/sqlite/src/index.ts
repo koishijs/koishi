2591a4b5a94d4034f1bd2cc9089771471c4723b9
Shigma <shigma10826@gmail.com>
2023-06-01 00:41:02 +0800

    feat(sqlite): migrate to standard data directory

--- plugins/database/sqlite/src/index.ts
+++ plugins/database/sqlite/src/index.ts
@@ -1,13 +1,20 @@
-import { defineDriver, Schema } from 'koishi'
+import { defineDriver, Logger, Schema } from 'koishi'
 import { SQLiteDriver } from '@minatojs/driver-sqlite'
+import fs from 'fs'
 import path from 'path'
 
+const logger = new Logger('sqlite')
+
 export default defineDriver(SQLiteDriver, Schema.object({
-  path: Schema.string().default('.koishi.db'),
+  path: Schema.path().default('data/koishi.db'),
 }).i18n({
   'zh-CN': require('./locales/zh-CN'),
 }), (ctx, config) => {
-  if (config.path !== ':memory:') {
-    config.path = path.resolve(ctx.baseDir, config.path)
+  if (config.path === ':memory:') return
+  config.path = path.resolve(ctx.baseDir, config.path)
+  const oldPath = path.resolve(ctx.baseDir, '.koishi.db')
+  if (fs.existsSync(oldPath)) {
+    logger.info('migrating to data directory')
+    fs.renameSync(oldPath, config.path)
   }
 })
