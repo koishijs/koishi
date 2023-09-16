949d81b6a2e34ba5c2de77ceb8d2f867c483b69b
Shigma <shigma10826@gmail.com>
2023-09-17 02:29:10 +0800

    chore: remove build directory

--- build/index.ts
+++
@@ -1,9 +0,0 @@
-import { BuildOptions } from 'esbuild'
-
-interface InputBuildOptions extends BuildOptions {
-  entryPoints: string[]
-}
-
-type DefineBuild = (base: string, options: InputBuildOptions) => void | Promise<void> | BuildOptions[] | Promise<BuildOptions[]>
-
-export const defineBuild = (callback: DefineBuild) => callback
