a98b000168f042d62fa9fc1f8c500cbed547e870
Shigma <shigma10826@gmail.com>
2023-11-14 02:50:51 +0800

    feat(mysql): support config ssl.rejectUnauthorized

--- plugins/database/mysql/src/index.ts
+++ plugins/database/mysql/src/index.ts
@@ -1,12 +1,19 @@
 import { defineDriver, Schema } from 'koishi'
 import { MySQLDriver } from '@minatojs/driver-mysql'
 
-export default defineDriver(MySQLDriver, Schema.object({
-  host: Schema.string().default('localhost'),
-  port: Schema.natural().max(65535).default(3306),
-  user: Schema.string().default('root'),
-  password: Schema.string().role('secret'),
-  database: Schema.string().default('koishi'),
-}).i18n({
+export default defineDriver(MySQLDriver, Schema.intersect([
+  Schema.object({
+    host: Schema.string().default('localhost'),
+    port: Schema.natural().max(65535).default(3306),
+    user: Schema.string().default('root'),
+    password: Schema.string().role('secret'),
+    database: Schema.string().default('koishi'),
+  }),
+  Schema.object({
+    ssl: Schema.object({
+      rejectUnauthorized: Schema.boolean().default(true),
+    }),
+  }),
+]).i18n({
   'zh-CN': require('./locales/zh-CN'),
 }))
